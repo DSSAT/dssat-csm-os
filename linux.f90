569059e4e074ecc56a295ac887c68f3965d045d5
Phillip D. Alderman <p.alderman@cgiar.org>
2014-04-03 17:00:49 -0700

    Added proper variable declaration to opsys() subroutine

--- linux.f90
+++ linux.f90
@@ -2,6 +2,10 @@
 
     implicit none
 
+    character(len=1) :: slash
+    character(len=12) :: dssatpro
+    character(len=30) :: stdpath
+
     SLASH = '/' 
     DSSATPRO = 'DSSATPRO.L46'
     STDPATH = '../DSSAT46/'
