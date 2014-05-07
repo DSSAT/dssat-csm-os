660cc13a3a311bbbea0bdeddd13fef7ec2c48a0d
Cheryl Porter <cporter@ufl.edu>
2014-05-07 09:20:45 -0400

    v4.6.0.36

--- ModuleDefs.for
+++ ModuleDefs.for
@@ -61,12 +61,13 @@ C             CHP Added TRTNUM to CONTROL variable.
         INTEGER :: Major = 4
         INTEGER :: Minor = 6
         INTEGER :: Model = 0
-        INTEGER :: Build = 35
+        INTEGER :: Build = 36
       END TYPE VersionType
       TYPE (VersionType) Version
       CHARACTER(len=10) :: VBranch = '-develop  '
 
 !     Version history:  
+!       4.6.0.36 chp 05/07/2014 SALUS model updates, ESCP, EPCP added to Summary.OUT
 !       4.6.0.35 chp 03/28/2014 Taro initialization fixed - RO
 !       4.6.0.34 chp 03/28/2014 Minor changes Weather, CSCER, sorghum
 !       4.6.0.33 chp 03/26/2014 Y2K crossover at 2020
