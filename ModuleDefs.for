9c48116720e4b457f41d95bfcfb424acb8ae7e1e
Cheryl Porter <cporter@ufl.edu>
2014-07-29 13:45:15 -0400

    v4.6.0.39 (addition of daily CO2 in weather file)

--- ModuleDefs.for
+++ ModuleDefs.for
@@ -61,12 +61,13 @@ C             CHP Added TRTNUM to CONTROL variable.
         INTEGER :: Major = 4
         INTEGER :: Minor = 6
         INTEGER :: Model = 0
-        INTEGER :: Build = 38
+        INTEGER :: Build = 39
       END TYPE VersionType
       TYPE (VersionType) Version
       CHARACTER(len=10) :: VBranch = '-develop  '
 
 !     Version history:  
+!       4.6.0.39 chp 07/25/2014 Allow daily input of CO2 in weather file (header CO2 or DCO2)
 !       4.6.0.38 chp 07/17/2014 Fixed output switches
 !       4.6.0.37 chp 06/05/2014 ORYZA code updated to ORYZA3 (Tao Li)
 !       4.6.0.36 chp 05/07/2014 SALUS model updates, ESCP, EPCP added to Summary.OUT
