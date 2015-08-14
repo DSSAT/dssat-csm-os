f6f436c5b15ac4fa9abf3d53ced135a9894b0bb8
Cheryl Porter <cporter@ufl.edu>
2015-08-14 14:47:17 -0400

    version number and branch

--- ModuleDefs.for
+++ ModuleDefs.for
@@ -60,14 +60,15 @@ C             CHP Added TRTNUM to CONTROL variable.
       TYPE VersionType
         INTEGER :: Major = 4
         INTEGER :: Minor = 6
-        INTEGER :: Model = 1
-        INTEGER :: Build = 0
+        INTEGER :: Model = 0
+        INTEGER :: Build = 50
       END TYPE VersionType
       TYPE (VersionType) Version
-      CHARACTER(len=10) :: VBranch = '-develop  '
+      CHARACTER(len=10) :: VBranch = '-NWheat   '
 !     CHARACTER(len=10) :: VBranch = '-release  '
 
 !     Version history:  
+!       4.6.0.50 chp 08/14/2015 NWheat brougt up to latest version
 !       4.6.1.00 GH  07/01/2015 DSSAT Version 4.6.1 Release
 !       4.6.0.49 GH  06/19/2015 CERES-Rice drought stress issue
 !       4.6.0.48 GH  06/18/2015 Harvest fix CERES & minor data file updates
