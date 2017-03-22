d396425c6cab6bc9076734930c497579d53a2bc6
vshelia <errtovsh@gmail.com>
2017-03-21 20:47:44 -0400

    Added logical unit number calculation
    Fixed issue related missing file number in open clause

--- CSVOUTS/csvgenericf.f90
+++ CSVOUTS/csvgenericf.f90
@@ -72,7 +72,8 @@ Contains
   //'PST1A,PST2A,KSTD,EWSD,LN%D,SH%D,HIPD,PWDD,PWTD,SLAD,CHTD,CWID,NWAD,RDPD,'&
   //'RL1D,RL2D,RL3D,RL4D,RL5D,RL6D,RL7D,RL8D,RL9D,SNW0C,SNW1C'
    
-       fn = 'plantgro.csv'      
+       fn = 'plantgro.csv' 
+       Call GETLUN (fn,nf)     
        Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW',  &
           Action='Write', IOSTAT = ErrNum)
         
@@ -98,7 +99,8 @@ Contains
   //'PREC,IR#C,IRRC,DTWT,MWTD,TDFD,TDFC,ROFD,SW1D,SW2D,SW3D,SW4D,SW5D,SW6D,' &
   //'SW7D,SW8D,SW9D' 
    
-       fn = 'soilwat.csv'      
+       fn = 'soilwat.csv'   
+       Call GETLUN (fn,nf)   
        Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
           Action='Write', IOSTAT = ErrNum)
         
@@ -122,7 +124,8 @@ Contains
   Header = 'RUN,EXP,TRTNUM,ROTNUM,REPNO,YEAR,DOY,DAS,TS0D,TS1D,TS2D,TS3D,' &
   //'TS4D,TS5D,TS6D,TS7D,TS8D,TS9D' 
    
-       fn = 'soiltemp.csv'      
+       fn = 'soiltemp.csv'  
+       Call GETLUN (fn,nf)    
        Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
           Action='Write', IOSTAT = ErrNum)
         
@@ -152,7 +155,8 @@ Contains
     //'RDPD,PTFD,SWXD,WAVRD,WUPRD,WFTD,WFPD,WFGD,NFTD,NFPD,NFGD,NUPRD,TFPD,' &
     //'TFGD,VRNFD,DYLFD' 
    
-       fn = 'plantgro.csv'      
+       fn = 'plantgro.csv'    
+       Call GETLUN (fn,nf)  
        Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
           Action='Write', IOSTAT = ErrNum)
         
@@ -179,7 +183,8 @@ Contains
   //'EOAA,EOPA,EOSA,ETAA,EPAA,ESAA,EFAA,EMAA,EOAC,ETAC,EPAC,ESAC,EFAC,' &
   //'EMAC,ES1D,ES2D,ES3D,ES4D,ES5D,ES6D,ES7D,ES8D,ES9D' 
    
-       fn = 'et.csv'      
+       fn = 'et.csv'   
+       Call GETLUN (fn,nf)   
        Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
           Action='Write', IOSTAT = ErrNum)
         
@@ -208,7 +213,8 @@ Contains
   //'PST2A,KSTD,LN%D,SH%D,HIPD,PWDD,PWTD,SLAD,CHTD,CWID,RDPD,RL1D,RL2D,RL3D,'&   
   //'RL4D,RL5D,RL6D,RL7D,RL8D,RL9D,CDAD,LDAD,SDAD,SNW0C,SNW1C,DTTD' 
    
-       fn = 'plantgro.csv'      
+       fn = 'plantgro.csv' 
+       Call GETLUN (fn,nf)     
        Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
           Action='Write', IOSTAT = ErrNum)
         
@@ -232,7 +238,8 @@ Contains
   Header = 'RUN,EXP,TR,RN,REP,YEAR,DOY,DAS,DAP,CNAD,GNAD,VNAD,GN%D,' &
   //'VN%D,NFXC,NUPC,LNAD,SNAD,LN%D,SN%D,SHND,RN%D,NFXD,SNN0C,SNN1C' 
    
-       fn = 'plantn.csv'      
+       fn = 'plantn.csv'     
+       Call GETLUN (fn,nf) 
        Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
           IOSTAT = ErrNum)
         
@@ -258,7 +265,8 @@ Contains
   //'RNAD,CNAD,LNAD,SNAD,HNAD,HIND,RSNAD,SNNPD,SNN0D,SNN1D,RN%D,LN%D,SN%D,' &
   //'HN%D,SDN%D,VN%D,LN%RD,SN%RD,RN%RD,VCN%,VMN%,NUPRD,NDEMD' 
    
-      fn = 'plantn.csv'      
+      fn = 'plantn.csv'  
+      Call GETLUN (fn,nf)    
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
         Action='Write', IOSTAT = ErrNum)
         
@@ -284,7 +292,8 @@ Contains
   //'NI1D,NI2D,NI3D,NI4D,NI5D,NI6D,NI7D,NI8D,NI9D,NH1D,NH2D,NH3D,NH4D,NH5D,' &
   //'NH6D,NH7D,NH8D,NH9D,NMNC,NITC,NDNC,NIMC,AMLC,NNMNC,NUCM' 
    
-      fn = 'soilni.csv'      
+      fn = 'soilni.csv'  
+      Call GETLUN (fn,nf)    
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
         Action='Write', IOSTAT = ErrNum)
         
@@ -308,7 +317,8 @@ Contains
   Header = 'RUN,EXP,TR,RN,REP,YEAR,DOY,DAS,DAP,CNAD,GNAD,VNAD,GN%D,VN%D,' &
   //'NUPC,LNAD,SNAD,LN%D,SN%D,RN%D,SNN0C,SNN1C' 
    
-      fn = 'plantn.csv'      
+      fn = 'plantn.csv' 
+      Call GETLUN (fn,nf)     
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
         Action='Write', IOSTAT = ErrNum)
         
@@ -332,7 +342,8 @@ Contains
   Header = 'RUN,EXP,TR,RN,REP,YEAR,DOY,DAS,PRED,DAYLD,TWLD,SRAD,' &
   //'PARD,CLDD,TMXD,TMND,TAVD,TDYD,TDWD,TGAD,TGRD,WDSD,CO2D' 
    
-      fn = 'weather.csv'      
+      fn = 'weather.csv' 
+      Call GETLUN (fn,nf)     
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
          Action='Write', IOSTAT = ErrNum)
         
@@ -358,7 +369,8 @@ Contains
   //'LAIPD,LAISD,LAID,CHTD,SDWAD,SNWLD,SNWSD,H#AD,HWUD,SHRTD,PTFD,RDPD,RL1D,' &
   //'RL2D,RL3D,RL4D,RL5D,RL6D,RL7D,RL8D,RL9D,RL10D' 
    
-      fn = 'plantgr2.csv'      
+      fn = 'plantgr2.csv' 
+      Call GETLUN (fn,nf)     
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW',  &
          Action='Write', IOSTAT = ErrNum)
         
@@ -384,7 +396,8 @@ Contains
   //'VRNFD,DYLFD,TFGEM,WFGE,TFPD,WFPD,NFPD,CO2FD,RSFPD,TFGD,WFGD,NFGD,WFTD,' &
   //'NFTD,WAVRD,WUPRD,SWXD,EOPD,SNXD,LN%RD,SN%RD,RN%RD' 
    
-      fn = 'plantgrf.csv'      
+      fn = 'plantgrf.csv'  
+      Call GETLUN (fn,nf)    
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
          Action='Write', IOSTAT = ErrNum)
         
@@ -412,7 +425,8 @@ Contains
   //'LAIXS,LAIXM,L#SMS,L#SMM,T#AMS,T#AMM,CWAMS,CWAMM,VWAMS,VWAMM,HIAMS,HIAMM,' &
   //'HN%MS,HN%MM,VN%MS,VN%MM,CNAMS,CNAMM,HNAMS,HNAMM,HINMS,HINMM' 
    
-      fn = 'evaluate.csv'      
+      fn = 'evaluate.csv'  
+      Call GETLUN (fn,nf)    
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
          Action='Write', IOSTAT = ErrNum)
         
@@ -446,7 +460,8 @@ Contains
 
       tmp1 = Header // ',' // trim(adjustl(tmp1))
    
-      fn = 'evaluate.csv'      
+      fn = 'evaluate.csv'  
+      Call GETLUN (fn,nf)    
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
          Action='Write', IOSTAT = ErrNum)
         
@@ -481,6 +496,7 @@ Contains
   // 'TMINA,SRADA,DAYLA,CO2A,PRCP,ETCP,ESCP,EPCP' 
    
        fn = 'summary.csv'      
+       Call GETLUN (fn,nf)
        Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
           Action='Write', IOSTAT = ErrNum)
         
@@ -505,7 +521,8 @@ Contains
   Header = 'RUN,EXP,TR,RN,REP,YEAR,DOY,DAS,DAP,TWAD,PHAD,CMAD,CGRD,' &
   //'GRAD,MRAD,CHAD,CL%D,CS%D,TGNN,TGAV,GN%D,GL%D,GC%D' 
    
-      fn = 'plantc.csv'      
+      fn = 'plantc.csv'
+      Call GETLUN (fn,nf)      
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', &
          Action='Write', IOSTAT = ErrNum)
         
@@ -545,7 +562,8 @@ Contains
          //'SOND,SN0D,SNTD,SOMNT,LNTD' 
       End If 
   
-      fn = 'soilorg.csv'      
+      fn = 'soilorg.csv' 
+      Call GETLUN (fn,nf)     
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', Action='Write', &
         IOSTAT = ErrNum)
      
@@ -570,7 +588,8 @@ Contains
      Header = 'RUN,EXP,TR,RN,REP,YEAR,DOY,DAS,' &
      //'LI%D,PHAD,PHAN,LI%N,SLLN,SLHN,N%LN,N%HN,LMLN,LMHN,TGNN,TGAV'    
   
-      fn = 'etphot.csv'      
+      fn = 'etphot.csv'  
+      Call GETLUN (fn,nf)    
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', Action='Write', &
          IOSTAT = ErrNum)
      
@@ -593,7 +612,8 @@ Contains
 
       Header = 'RUN,EXP,TR,RN,REP,YEAR,DOY,DAS,MCFD,MDEPD,MWAD,MWTD'    
   
-      fn = 'mulch.csv'      
+      fn = 'mulch.csv' 
+      Call GETLUN (fn,nf)     
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', Action='Write', &
          IOSTAT = ErrNum)
      
@@ -622,7 +642,8 @@ Contains
      //'SDPPD,PLPPD,SHPAD,RTPAD,SLPAD,SDPAD,PLPAD,PST1A,PST2A,PUPD,PUPC,' &
      //'SNP0C,SNP1C,PHFR1,PHFR2,SHWAD,RWAD,SHAD,GWAD,PSTRAT,NTOPD,PTDD'  
   
-      fn = 'plantp.csv'      
+      fn = 'plantp.csv'  
+      Call GETLUN (fn,nf)    
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', Action='Write', &
          IOSTAT = ErrNum)
      
@@ -651,7 +672,8 @@ Contains
      //'PUPC,PAV1D,PAV2D,PAV3D,PAV4D,PAV5D,PUP1D,PUP2D,PUP3D,' &
      //'PUP4D,PUP5D,PLAB1,PLAB2,PLAB3,PLAB4,PLAB5'    
   
-      fn = 'soilpi.csv'      
+      fn = 'soilpi.csv'
+      Call GETLUN (fn,nf)      
       Open (UNIT = nf, FILE = fn, FORM='FORMATTED', STATUS = 'NEW', Action='Write', &
          IOSTAT = ErrNum)
      
