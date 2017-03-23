92014fdd7465005f06fbbf3fe02c999bc99f032b
cporter <cporter@ufl.edu>
2017-03-22 21:32:38 -0400

    NUPTAK improved based on MZ_NUPTAK

--- Aloha/Aloha_NUPTAK.for
+++ Aloha/Aloha_NUPTAK.for
@@ -23,66 +23,73 @@ C  Called : GROSUB SGROSUB MGROSUB WGROSUB
 C
 C  Calls  : None
 C-----------------------------------------------------------------------
-C                         DEFINITIONS
-C
-C  ANDEM  : Crop N demand (kg N/ha)
-C  ANH4   : Total extractable ammonium N in soil profile (kg N/ha)
-C  ANO3   : Total extractable nitrate N in soil profile (kg N/ha)
-C  DNG    : N demand of potential new growth of tops (g N/plant)
-C  DROOTN : Daily change in plant root nitrogen content (g N/plant)
-C  DSTOVN :
-C  FACTOR : Relative weighting to distribute crop root residues at the beginning
-C           of a simulation
-C  FNH4   : Unitless soil ammonium supply index
-C  FNO3   : Unitless soil nitrate supply index
-C  L,L1   : Loop counters
-C  NDEM   : Plant nitrogen demand (g/plant)
-C  NUF    : Plant N supply/demand ratio used to modify uptake
-C  RFAC   : Interim variable describing the effects of root length density
-C           on potential N uptake from a layer
-C  RNDEM  : Plant root demand for nitrogen (g/plant)
-C  RNH4U  : Potential ammonium uptake from Layer L (kg N/ha)
-C  RNLOSS : Loss of N from the plant via root exudation in one layer (g N/plant)
-C  RNO3U  : Potential nitrate uptake from Layer L (kg N/ha)
-C  SMDFR  : Soil moisture deficit factor affecting N uptake
-C  THUMN  :
-C  TNDEM  : Plant tops demand for nitrogen (g N/plant)
-C  TOTN   : Total mineral N in a layer (kg N/ha)
-C  TRLV   : Total root length density variable
-C  TRNLOS : Total plant N lost by root exudation (g N/plant)
-C  UNH4   : Plant uptake of ammonium from a layer (kg N/ha)
-C  UNO3   : Plant uptake of nitrate from a layer (kg N/ha)
-C  XMIN   :
-C  XNDEM  :
 C=======================================================================
 
-      SUBROUTINE Aloha_NUPTAK
-
+      SUBROUTINE Aloha_NUPTAK()
+      USE ModuleDefs
       IMPLICIT  NONE
-
-      INCLUDE  'GEN1.BLK'
-      INCLUDE  'GEN3.BLK'
-      INCLUDE  'GEN4.BLK'
-      INCLUDE  'NTRC1.BLK'
-      INCLUDE  'NTRC2.BLK'
-
-      REAL      NUF,NDEM
-      DIMENSION RNO3U(20),RNH4U(20)
-
-      INTEGER   L,L1
-      REAL      RNH4U,RNO3U,TRNLOS,TRLV,DNG,
-     +          TNDEM,RNDEM,ANDEM,DROOTN,DSTOVN,FNH4,FNO3,SMDFR,RFAC,
-     +          UNO3,UNH4,XMIN,RNLOSS,XNDEM,FACTOR
-
       SAVE
+
+      !INCLUDE  'GEN1.BLK'
+      !INCLUDE  'GEN3.BLK'
+      !INCLUDE  'GEN4.BLK'
+      !INCLUDE  'NTRC1.BLK'
+      !INCLUDE  'NTRC2.BLK'
+
+      REAL, DIMENSION(NL) :: ANO3, ANH4, DLAYR, KG2PPM, LL, NH4, NO3, 
+     &     RLV, RNO3U, RNH4U, SAT, SHF, SNH4, SNO3, SW, UNH4, UNO3
+
+      REAL        ANDEM              
+      REAL        DNG         
+      REAL        DROOTN      
+      REAL        DSTOVN      
+      REAL        FACTOR      
+      REAL        FNH4        
+      REAL        FNO3        
+      INTEGER     L           
+      INTEGER     L1          
+      REAL        NDEM        
+      REAL        NUF
+      INTEGER     NLAYR       
+      REAL        PDWI        
+      REAL        PGRORT      
+      REAL        PLTPOP      
+      REAL        PTF         
+      REAL        RANC        
+      REAL        RCNP        
+      REAL        RFAC
+      REAL        RNDEM       
+      REAL        RNLOSS      
+      REAL        ROOTN       
+      REAL        RTWT
+      REAL        SMDFR       
+      REAL        STOVN       
+      REAL        STOVWT      
+      REAL        TANC        
+      REAL        TCNP        
+      REAL        TNDEM       
+      REAL        TRLV        
+      REAL        TRNLOS      
+      REAL        TRNU        
+      REAL        XMIN        
+      REAL        XNDEM       
+      REAL        XSTAGE      
+
+      TYPE (ResidueType) SENESCE
+
+!----------------------------------------------------------------------
+!     CHP 3/30/2006
+!     Proportion of lignin in roots
+      REAL PLIGRT
+
 
 C-----------------------------------------------------------------------
 C     Initialize variables
 C-----------------------------------------------------------------------
 
       TRNLOS = 0.0
-      RANC   = ROOTN / RTWT
-      TANC   = STOVN / STOVWT
+      IF (RTWT.GT.0.0) RANC   = ROOTN / RTWT
+      IF (STOVWT.GT.0.0) TANC   = STOVN / STOVWT
       TRLV   = 0.0
       ANO3   = 0.0
       ANH4   = 0.0
@@ -90,13 +97,32 @@ C-----------------------------------------------------------------------
       DSTOVN = 0.0
       TRNU   = 0.0
       NUF    = 0.0
+      XNDEM = 0.0
+      XMIN = 0.0
+      TNDEM = 0.0
+      RNLOSS = 0.0
+      RNDEM = 0.0
+      RFAC = 0.0
+      NDEM = 0.0
+      FNO3 = 0.0
+      FNH4 = 0.0
+      FACTOR = 0.0
+      DNG = 0.0
+      ANDEM = 0.0
+      L1 = 0
+      UNH4 = 0.0
+      UNO3 = 0.0
 
       DO L = 1, NLAYR
          RNO3U(L) = 0.0
          RNH4U(L) = 0.0
-         TRLV     = TRLV    + RLV(L)
-         NO3(L)   = SNO3(L) * FAC(L)
-         NH4(L)   = SNH4(L) * FAC(L)
+!         TRLV     = TRLV    + RLV(L)
+         TRLV     = TRLV    + RLV(L) * DLAYR(L)
+!        NO3(L)   = SNO3(L) * FAC(L)
+!        NH4(L)   = SNH4(L) * FAC(L)
+         !KG2PPM(L) = 10.0/(BD(L)*DLAYR(L))
+         SNO3(L) = NO3(L) / KG2PPM(L) 
+         SNH4(L) = NH4(L) / KG2PPM(L)
       END DO
 
 C-----------------------------------------------------------------------
@@ -131,7 +157,7 @@ C     Calculate potential N supply in soil layers with roots (TRNU)
 C-----------------------------------------------------------------------
 
       DO L = 1, NLAYR
-         IF (RLV(L) .NE. 0.0) THEN
+         IF (RLV(L) > 0.0001) THEN
             L1 = L
 C
 C           The following code was written by JTR to simplify the code for the
@@ -149,6 +175,8 @@ C
             FNO3     = SHF(L)*0.075
             RNH4U(L) = RFAC*SMDFR*FNH4*(NH4(L)-0.5)*DLAYR(L)
             RNO3U(L) = RFAC*SMDFR*FNO3*NO3(L)*DLAYR(L)
+            RNH4U(L) = MAX(RNH4U(L),0.0)
+            RNO3U(L) = MAX(RNO3U(L),0.0) 
             TRNU     = TRNU + RNO3U(L) + RNH4U(L)
          ENDIF
       END DO
@@ -172,21 +200,26 @@ C     Calculate N uptake in soil layers with roots based on demand (kg/ha)
 C-------------------------------------------------------------------------
 
       DO L = 1, L1
-         UNO3    = RNO3U(L)*NUF
-         UNH4    = RNH4U(L)*NUF
-         XMIN    = 0.25/FAC(L)
-         UNO3    = MIN (UNO3,SNO3(L) - XMIN)
-         SNO3(L) = MAX (SNO3(L) - UNO3, 0.0)
-         XMIN    = 0.5/FAC(L)
-         UNH4    = MIN (UNH4,SNH4(L) - XMIN)
-         SNH4(L) = MAX (SNH4(L) - UNH4, 0.0)
-         NO3(L)  = SNO3(L) * FAC(L)
-         NH4(L)  = SNH4(L) * FAC(L)
-         TRNU    = TRNU + UNO3 + UNH4
+         UNO3(L) = RNO3U(L)*NUF
+         UNH4(L) = RNH4U(L)*NUF
+         XMIN    = 0.25/KG2PPM(L)
+         UNO3(L) = MIN (UNO3(L),SNO3(L) - XMIN)
+         UNO3(L) = MAX(UNO3(L),0.0)  
+         SNO3(L) = MAX (SNO3(L) - UNO3(L), 0.0)
+         XMIN    = 0.5/KG2PPM(L)
+         UNH4(L) = MIN (UNH4(L),SNH4(L) - XMIN)
+         UNH4(L) = MAX(UNH4(L),0.0) 
+         SNH4(L) = MAX (SNH4(L) - UNH4(L), 0.0)
+!        NO3(L)  = SNO3(L) * FAC(L)
+!        NH4(L)  = SNH4(L) * FAC(L)
+         TRNU    = TRNU + UNO3(L) + UNH4(L)   !kg[N]/ha
       END DO
 
-      TRNU = TRNU/(PLTPOP*10.0)
-
+      IF (PLTPOP .GT. 0.0) THEN
+        TRNU = TRNU/(PLTPOP*10.0)               !g[N]/plant
+      ELSE
+        TRNU = 0.0
+      ENDIF
 C-----------------------------------------------------------------------
 C     Update stover and root N
 C-----------------------------------------------------------------------
@@ -211,20 +244,92 @@ C-----------------------------------------------------------------------
             IF (TANC .GT. TCNP) THEN
                RNLOSS = RANC*RTWT*0.05*PLTPOP*RLV(L)/TRLV
             ENDIF
+            !Compute lignin, cellulose and carbohydrate portions
+            SENESCE % ResLig(L) = SENESCE % ResWt(L) * PLIGRT
+
             TRNLOS  = TRNLOS + RNLOSS
-            FON(L)  = FON(L) + RNLOSS
+            !FON(L)  = FON(L) + RNLOSS     !FON is local variable here
          END DO
-         !
+
          ! Adjust DSTOVN and DROOTN to compensate for N lost to FON
-         !
-         DSTOVN = TNDEM / NDEM*TRNU-      PTF*TRNLOS/(PLTPOP*10.0)
-         DROOTN = RNDEM / NDEM*TRNU-(1.0-PTF)*TRNLOS/(PLTPOP*10.0)
+         IF(NDEM.GT.0.0.AND.PLTPOP.GT.0.0) THEN
+!           DSTOVN = TNDEM / NDEM*TRNU-      PTF*TRNLOS/(PLTPOP*10.0)
+!           DROOTN = RNDEM / NDEM*TRNU-(1.0-PTF)*TRNLOS/(PLTPOP*10.0)
+!     JIL 06/25/2007
+           DSTOVN = TNDEM / NDEM*TRNU-      PTF*TRNLOS/(PLTPOP)
+           DROOTN = RNDEM / NDEM*TRNU-(1.0-PTF)*TRNLOS/(PLTPOP)
+!          g N/pl =   fraction  *g N/pl- fraction*g N/m2/(pl/m2) 
+         ENDIF
       ENDIF
 
       STOVN = STOVN + DSTOVN
-      TANC  = STOVN / STOVWT
+      IF(STOVWT.GT.0.0) TANC  = STOVN / STOVWT
       ROOTN = ROOTN + DROOTN
-      RANC  = ROOTN / (RTWT-0.01*RTWT)
-
+      IF(RTWT.GT.0.1*RTWT.AND.RTWT.GT.0.0) 
+     &   RANC  = ROOTN / (RTWT-0.01*RTWT)
+      
       RETURN
       END SUBROUTINE Aloha_NUPTAK
+
+
+C--------------------------------------------------------------------------------------------------
+C                         Define Variables
+C--------------------------------------------------------------------------------------------------
+
+! ANDEM       !Crop N demand (kg N/ha)
+! ANH4        !Total extractable ammonium N in soil profile (kg N/ha)
+! ANO3        !Total extractable nitrate N in soil profile (kg N/ha)
+! DLAYR(L)    Soil thickness in layer L (cm)
+! DNG         !N demand of potential new growth of tops (g N/plant)
+! DROOTN      !Daily change in plant root nitrogen content (g N/plant)
+! DSTOVN      !Daily change in plant stover nitrogen content (g N/plant)
+! FACTOR      !Ratio of root N uptake to plant N demand
+C  FACTOR : Relative weighting to distribute crop root residues at the beginning
+C           of a simulation
+! FNH4        !Unitless soil ammonium supply index
+! FNO3        !Unitless soil nitrate supply index
+! FON(20)     !Fresh organic nitrogen in soil layer L due to root senescence, kg N/ha
+! KG2PPM(20)  !Factor that convergs mg elemental N/kg soil to kg N/ha for soil layer L
+! L           !Index counter
+! L1          !Lowest soil layer with roots
+! LL(20)      !Lower limit of plant extractable water for soil layer L, cm3/cm3
+! NDEM        !Plant nitrogen demand, g/plant
+! NH4(20)     !Ammonium in soil layer L, mg elemental N/kg soil
+! NLAYR       !Number of soil layer
+! NO3(20)     !Nitrate in soil layer L (mg elemental N/kg soil)
+! NUF         !Plant N supply to demand ratio used to modify N uptake
+! PDWI        !Potential increment in new shoot growth, g/plant
+! PGRORT      !Potential increment in new root growth, g/plant
+! PLTPOP      !Plant population, plants/m2
+! PTF         !Ratio of above ground biomass to total biomass
+! RANC        !Root actual N concentration, g N/g root
+! RCNP        !Root critical nitrogen concentration, g N/g root dry weight
+!  RFAC       !Interim variable describing the effects of root length density
+!              on potential N uptake from a layer
+! RNDEM       !Plant root demand for nitrogen (g/plant)
+! RNH4U(20)   !Potential ammonia uptake from layer L, kg N/ha
+! RNLOSS      !Loss of N from the plant via root exudation in one layer (g N/m2)
+! RNO3U(20)   !Potential nitrate uptake from layer L, kg N/ha
+! ROOTN       !Root nitrogen content, g N/plant
+! RTWT        !Root weight, g/plant
+! SHF(20)     !Relative root distribution in soil layer L (0-1)
+! SMDFR       !Soil moisture deficit factor affecting N uptake
+! SNH4(20)    !Ammonium nitrogen in soil layer L, kg N/ha
+! SNO3(20)    !Nitrate content in soil layer L, kg N/ha
+! STOVN       !Nitrogen content in stover, g N/plant
+! STOVWT      !Stover weight (Stem + leaf), g/plant
+! SAT(20)     !Saturated water holding capacity for soil layer L, cm3/cm3
+! SW(20)      !Soil water content in layer L, cm3 water/cm3 soil
+! TANC        !Nitrogen content in above ground biomass, decimal
+! TCNP        !Critical nitrogen concentration in tops, g N/g dry weight
+! TNDEM       !Plant tops demand for nitrogen (g N/plant)
+C  TOTN   : Total mineral N in a layer (kg N/ha)
+! TRLV        !Total root length density, cm root/cm2 soil
+! TRNLOS      !Total plant N lost by root exudation (g N/m2)
+! TRNU        !Total potential root nitrogen uptake, kg N/ha
+! UNH4(20)    !Plant uptake of ammonium from layer (kg N/ha/day)
+! UNO3(20)    !Plant uptake of nitrate from a layer (kg N/ha/day)
+! XMIN        !
+! XNDEM       !Temporary variable
+! XSTAGE      !Non-integer growth stage indicator
+C  XNDEM  :
