3362a33f9cc7adcd88ea082c4ae0224381822991
Daniel Amariles <daniel.amariles88@gmail.com>
2017-04-07 09:40:30 -0500

    issue #46 separating static and object functions for Environment

--- CS_Model_Environment.f90
+++ CS_Model_Environment.f90
@@ -3,8 +3,8 @@
         
         real, private :: HOURS_OF_DAY = 24
         real, private :: HOURS_OF_LIGHT = 8
-        real, private :: TMin_ = 0 !
-        real, private :: TMax_ = 0 ! 
+        real, private :: tMin_ = 0 !
+        real, private :: tMax_ = 0 ! 
         
     contains
     
@@ -12,9 +12,9 @@
         procedure, pass (this) :: setTMin
         procedure, pass (this) :: getTMax
         procedure, pass (this) :: setTMax
-        procedure, pass (this) :: fetchTemperature
-        procedure, pass (this) :: fetchSVP
-        procedure, pass (this) :: fetchWaterHoldingCapacity
+        procedure, pass (this) :: getTemperature
+        procedure, pass (this) :: getSVP
+        procedure, pass (this) :: getWaterHoldingCapacity
     
     end Type DailyEnvironment_type
     
@@ -26,48 +26,53 @@
     contains
     
     ! constructor for the type
-    type (DailyEnvironment_type) function DailyEnvironment_type_constructor(TMin, TMax)
+    type (DailyEnvironment_type) function DailyEnvironment_type_constructor(tMin, tMax)
         implicit none
-        real, intent (in) :: TMin, TMax
-        DailyEnvironment_type_constructor%TMin_ = TMin
-        DailyEnvironment_type_constructor%TMax_ = TMax
+        real, intent (in) :: tMin, tMax
+        DailyEnvironment_type_constructor%TMin_ = tMin
+        DailyEnvironment_type_constructor%TMax_ = tMax
     end function DailyEnvironment_type_constructor    
-
+    
+    !-------------------------------------------
+    ! GETTERS AND SETTERS
+    
     ! get TMin
     real function getTMin(this)
         implicit none
         class (DailyEnvironment_type), intent(in) :: this
         
-        getTMin = this%TMin_
+        getTMin = this%tMin_
     end function getTMin
     
     ! set TMin    
-    subroutine setTMin(this, TMin)
+    subroutine setTMin(this, tMin)
         implicit none
         class (DailyEnvironment_type), intent(inout) :: this
-        real, intent (in) :: TMin
+        real, intent (in) :: tMin
         
-        this%TMin_ = TMin
+        this%tMin_ = tMin
     end subroutine setTMin
     
-    ! get TMax
+    ! get tMax
     real function getTMax(this)
         implicit none
         class (DailyEnvironment_type), intent(in) :: this
         
-        getTMax = this%TMax_
+        getTMax = this%tMax_
     end function getTMax
     
     
-    ! set TMax    
-    subroutine setTMax(this, TMax)
+    ! set tMax    
+    subroutine setTMax(this, tMax)
         implicit none
         class (DailyEnvironment_type), intent(inout) :: this
-        real, intent (in) :: TMax
+        real, intent (in) :: tMax
         
-        this%TMax_ = TMax
+        this%tMax_ = tMax
     end subroutine setTMax
     
+    !-------------------------------------------
+    ! OBJECT FUNCTIONS
     
     ! obtain the temperature for any given hour of the day
     ! T(Hour) = Amplitude*sin[w(t - a)] + C.
@@ -76,7 +81,7 @@
     ! a  is the phase shift (the horizontal offset of the basepoint; where the curve crosses the baseline as it ascends)
     ! C is average temperature,  the vertical offset (height of the baseline) 
     ! w is the angular frequency, given by w = 2PI/hod 
-    real function fetchTemperature(this, hour)
+    real function getTemperature(this, hour)
         implicit none
         class (DailyEnvironment_type), intent(in) :: this
         integer, intent (in) :: hour
@@ -90,39 +95,50 @@
         g = w*(hour - this%HOURS_OF_LIGHT)
 
         
-        fetchTemperature = Amplitude*SIN(g)+C           ! calculate temperature acording to the current time
+        getTemperature = Amplitude*SIN(g)+C           ! calculate temperature acording to the current time
 
-    end function fetchTemperature
+    end function getTemperature
     
-    ! obtain the Saturation Vapour Pressure (pascals) for any given temperature
-    real function fetchSVP(this, temperature)
+    
+    !obtain the Saturation Vapour Pressure (pascals) for any given hour
+    real function getSVP(this, Hour)
         implicit none
         class (DailyEnvironment_type), intent(in) :: this
-        real, intent (in) :: temperature
+        integer, intent (in) :: Hour
         
-        fetchSVP = 610.78 * exp( temperature/ ( temperature  + 238.3 ) * 17.2694 )        !  DA Saturation vapour pressure in pascals: svp = 610.78 *exp( t / ( t + 238.3 ) *17.2694 ) 
+        getSVP = calculateSVP(getTemperature(this, Hour))                   !  DA Saturation vapour pressure in pascals: svp = 610.78 *exp( t / ( t + 238.3 ) *17.2694 ) 
 
-    end function fetchSVP
+    end function getSVP
     
     ! obtain the water holding capacity of the air (kg/m3) for any given temperature and SVP
-    real function fetchWaterHoldingCapacity(this, temperature, SVP)
+    real function getWaterHoldingCapacity(this, hour)
         implicit none
         class (DailyEnvironment_type), intent(in) :: this
+        integer, intent (in) :: Hour
+        
+        getWaterHoldingCapacity = calculateWaterHoldingCapacity(getTemperature(this, Hour), getSVP(this, Hour))                                !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
+
+    end function getWaterHoldingCapacity
+   
+    !-------------------------------------------
+    ! STATIC FUNCTIONS
+    ! obtain the Saturation Vapour Pressure (pascals) for any given temperature
+    real function calculateSVP(temperature)
+        implicit none
+        real, intent (in) :: temperature
+        
+        calculateSVP = 610.78 * exp( temperature/ ( temperature  + 238.3 ) * 17.2694 )        !  DA Saturation vapour pressure in pascals: svp = 610.78 *exp( t / ( t + 238.3 ) *17.2694 ) 
+
+    end function calculateSVP
+    
+        ! obtain the water holding capacity of the air (kg/m3) for any given temperature and SVP
+    real function calculateWaterHoldingCapacity(temperature, SVP)
+        implicit none
         real, intent (in) :: temperature, SVP
         
-        fetchWaterHoldingCapacity = 0.002166 * SVP / ( temperature  + 273.16 )                                !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
+        calculateWaterHoldingCapacity = 0.002166 * SVP / ( temperature  + 273.16 )                                !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
 
-    end function fetchWaterHoldingCapacity
+    end function calculateWaterHoldingCapacity
     
-    ! obtain the incoming radiation at the given hour
-    !real function fetchIncomingRadiation(this)
-    !    implicit none
-    !    class (DailyEnvironment_type), intent(in) :: this
-    !    
-    !    fetchIncomingRadiation = Radiation * (Tmax_- Tmin_)
-    !
-    !end function fetchIncomingRadiation
-
-        
 END Module CS_Model_Environment    
     
