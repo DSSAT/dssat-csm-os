0a6ee6a08f5f47ebdd174d1b8448365ca386fa66
Daniel Amariles <daniel.amariles88@gmail.com>
2017-04-18 15:43:23 -0500

    adding relative transpiration calculation

--- CS_Model_Environment.f90
+++ CS_Model_Environment.f90
@@ -12,11 +12,17 @@
 !        hourlyRH
 !        hourlyVPD
 !        hourlyRadiation
+!        hourlyBiomass
+!        hourlyTranspiration
 ! Static functions:
 !        calculateSVP
 !        calculateWHCAIR
 !        calculateRH
 !        calculateVPD
+!        calculatePortionOfRadiation
+!        calculatateHourlyRadiation
+!        calculateBiomass
+!        calculateTranspiration
 ! Authors
 ! @danipilze
 !*********
@@ -33,10 +39,10 @@
         
         ! OBJECT ATRIBUTES
         
-        real, private :: tMin_ = 0 !
-        real, private :: tMax_ = 0 ! 
-        real, private :: dewPoint_ = 0 !
-        real, private :: dayRadiation_ = 0 ! solar radiation
+        real, private :: tMin_ = 0
+        real, private :: tMax_ = 0
+        real, private :: dewPoint_ = 0
+        real, private :: dayRadiation_ = 0                                                      ! solar radiation
         
         
     contains
@@ -56,6 +62,7 @@
         procedure, pass (this) :: hourlyVPD
         procedure, pass (this) :: hourlyRadiation
         procedure, pass (this) :: hourlyBiomass
+        procedure, pass (this) :: hourlyTranspiration
         
     
     end Type DailyEnvironment_type
@@ -101,43 +108,43 @@
         real :: Amplitude, C, w, g
 
         w = (2*PI)/HOURS_OF_DAY
-        Amplitude = ((this%TMax_ - this%TMin_)/2)           ! half distance between temperatures
-        C = (this%TMin_ + this%TMax_)/2                     ! mean temperature
+        Amplitude = ((this%TMax_ - this%TMin_)/2)                                               ! half distance between temperatures
+        C = (this%TMin_ + this%TMax_)/2                                                         ! mean temperature
         g = w*(hour - 8)
 
         
-        hourlyTemperature = Amplitude*SIN(g)+C           ! calculate temperature acording to the current time
+        hourlyTemperature = Amplitude*SIN(g)+C                                                  ! calculate temperature acording to the current time
 
     end function hourlyTemperature
     
     
-    !obtain the Saturation Vapour Pressure (pascals) for any given hour
-    real function hourlySVP(this, Hour)
+    !obtain the Saturation Vapour Pressure for any given hour                                   ! (pascals)
+    real function hourlySVP(this, Hour)  
         implicit none
         class (DailyEnvironment_type), intent(in) :: this
         integer, intent (in) :: hour
         
-        hourlySVP = calculateSVP(hourlyTemperature(this, Hour))                   !  DA Saturation vapour pressure in pascals: svp = 610.78 *exp( t / ( t + 238.3 ) *17.2694 ) 
+        hourlySVP = calculateSVP(hourlyTemperature(this, Hour))
 
     end function hourlySVP
     
-    ! obtain the water holding capacity of the air (kg/m3) for any given hour
-    real function hourlyWHCAIR(this, hour)
+    ! obtain the water holding capacity of the air for any given hour
+    real function hourlyWHCAIR(this, hour) ! (kg/m3)
         implicit none
         class (DailyEnvironment_type), intent(in) :: this
         integer, intent (in) :: hour
         
-        hourlyWHCAIR = calculateWHCAIR(hourlyTemperature(this, Hour))                                !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
+        hourlyWHCAIR = calculateWHCAIR(hourlyTemperature(this, Hour))                           !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
 
     end function hourlyWHCAIR
     
     ! obtain the relative humidity for any given hour
-    real function hourlyRH(this, hour)
+    real function hourlyRH(this, hour) 
         implicit none
         class (DailyEnvironment_type), intent(in) :: this
         integer, intent (in) :: hour
         
-        hourlyRH =  calculateRH(hourlyTemperature(this, Hour), this%dewpoint_)                               !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
+        hourlyRH =  calculateRH(hourlyTemperature(this, Hour), this%dewpoint_)                  !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
 
     end function hourlyRH
     
@@ -147,7 +154,7 @@
         class (DailyEnvironment_type), intent(in) :: this
         integer, intent (in) :: hour
         
-        hourlyVPD =  calculateVPD(hourlyTemperature(this, Hour), this%dewpoint_)                               !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
+        hourlyVPD =  calculateVPD(hourlyTemperature(this, Hour), this%dewpoint_)                !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
 
     end function hourlyVPD
     
@@ -160,11 +167,10 @@
                 
         hourlyRadiation = calculatateHourlyRadiation(hour, DAWN_TIME, this%dayRadiation_ , LIGHT_HOURS) 
         
-        
     end function hourlyRadiation
     
     ! obtain the biomass produced at a given hour
-    real function hourlyBiomass(this, hour, extinctionCoefficient,LAI, radiationUseEfficiency,stomatalConductance )
+    real function hourlyBiomass(this, hour, extinctionCoefficient,LAI, radiationUseEfficiency,stomatalConductance)
         implicit none
         class (DailyEnvironment_type), intent(in) :: this
         integer, intent (in) :: hour
@@ -172,39 +178,48 @@
                
         hourlyBiomass = calculateBiomass(calculatateHourlyRadiation(hour, DAWN_TIME, this%dayRadiation_ , LIGHT_HOURS) , extinctionCoefficient , LAI, radiationUseEfficiency, stomatalConductance )
         
-        
     end function hourlyBiomass
+    
+    ! obtain the transpiration
+    real function hourlyTranspiration(this, hour, stomatalConductance)
+        implicit none
+        class (DailyEnvironment_type), intent(in) :: this
+        real, intent (in) :: hour, stomatalConductance
+        
+        hourlyTranspiration = calculateTranspiration(hourlyVPD(this, hour), stomatalConductance)
+
+    end function hourlyTranspiration
    
     !-------------------------------------------
     ! STATIC FUNCTIONS
     !-------------------------------------------
     
-    ! obtain the Saturation Vapour Pressure (pascals) for any given temperature
-    real function calculateSVP(temperature)
+    ! obtain the Saturation Vapour Pressure  for any given temperature                          ! (pascals)
+    real function calculateSVP(temperature) 
         implicit none
         real, intent (in) :: temperature
         
-        calculateSVP = 610.78 * exp( temperature/ ( temperature  + 238.3 ) * 17.2694 )        !  DA Saturation vapour pressure in pascals: svp = 610.78 *exp( t / ( t + 238.3 ) *17.2694 ) 
+        calculateSVP = 610.78 * exp( temperature/ ( temperature  + 238.3 ) * 17.2694 )          !  DA Saturation vapour pressure in pascals: svp = 610.78 *exp( t / ( t + 238.3 ) *17.2694 ) 
 
     end function calculateSVP
     
-    ! obtain the water holding capacity of the air (kg/m3) for any given temperature
-    real function calculateWHCAIR(temperature)
+    ! obtain the water holding capacity of the air for any given temperature                    ! (kg/m3)
+    real function calculateWHCAIR(temperature) 
         implicit none
         real, intent (in) :: temperature
         
-        ! WHCAIR = 0.002166 *SVP / ( t + 273.16 )   
-        calculateWHCAIR = 0.002166 * calculateSVP(temperature) / ( temperature  + 273.16 )                                !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
+        ! WHCAIR = 0.002166 *SVP / ( t + 273.16 )
+        calculateWHCAIR = 0.002166 * calculateSVP(temperature) / ( temperature  + 273.16 )      !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
 
     end function calculateWHCAIR
      
-    ! obtain the relative humidity 
+    ! obtain the relative humidity                                                              
     real function calculateRH(temperature, dewPoint)
         implicit none
         real, intent (in) :: temperature, dewPoint
         
         !(RHt) = WHCAIRdp/WHCAIRt
-        calculateRH =  calculateWHCAIR(dewPoint)/calculateWHCAIR(temperature)                              !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
+        calculateRH =  calculateWHCAIR(dewPoint)/calculateWHCAIR(temperature)                   !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
 
     end function calculateRH
     
@@ -213,8 +228,8 @@
         implicit none
         real, intent (in) :: temperature, dewPoint
         
-        ! VPD = (1 - (RH/100)) * SVP 
-        calculateVPD =  ((1 - (calculateRH(temperature, dewPoint)/100)) * calculateSVP(temperature))/1000                               !  DA water holding capacity of the air WHC = 0.002166 * SVP / ( t + 273.16 )   
+        ! VPD = (1 - (RH/100)) * SVP
+        calculateVPD =  ((1 - (calculateRH(temperature, dewPoint)/100)) * calculateSVP(temperature))/1000
 
     end function calculateVPD
     
@@ -226,7 +241,7 @@
         real, intent (in) :: K, LAI
         real :: value = 0
         
-        calculatePortionOfRadiation = 1 - exp(- K*LAI)
+        calculatePortionOfRadiation = 1 - exp(-K*LAI)
 
     end function calculatePortionOfRadiation
     
@@ -249,7 +264,7 @@
     end function calculatateHourlyRadiation
     
     ! obtain the biomass produced accoording to the radiation at a given moment
-    real function calculateBiomass(radiation, extinctionCoefficient , LAI, radiationUseEfficiency, stomatalConductance )
+    real function calculateBiomass(radiation, extinctionCoefficient , LAI, radiationUseEfficiency, stomatalConductance)
         implicit none
         real, intent (in) :: radiation, extinctionCoefficient, LAI, radiationUseEfficiency, stomatalConductance
         real :: value = 0
@@ -257,6 +272,16 @@
         calculateBiomass = calculatePortionOfRadiation(extinctionCoefficient, LAI) * radiation * radiationUseEfficiency  * stomatalConductance
 
     end function calculateBiomass
+    
+    ! obtain the transpiration
+    real function calculateTranspiration(VPD, stomatalConductance)
+        implicit none
+        real, intent (in) :: stomatalConductance, VPD
+        real :: value = 0
+        
+        calculateTranspiration = stomatalConductance * VPD
+
+    end function calculateTranspiration
     
     !-------------------------------------------
     ! GETTERS AND SETTERS
