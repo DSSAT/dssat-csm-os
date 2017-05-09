ac99c56345120b5000a4ed4d0ff7f0b70c6d7a67
Cheryl Porter <cporter@ufl.edu>
2017-05-09 09:15:51 -0400

    Change max number of output files

--- ModuleDefs.for
+++ ModuleDefs.for
@@ -175,7 +175,7 @@ C             CHP Added TRTNUM to CONTROL variable.
      &    NumOfDays = 1000, !Maximum days in sugarcane run (FSR)
      &    NumOfStalks = 42, !Maximum stalks per sugarcane stubble (FSR)
      &    EvaluateNum = 40, !Number of evaluation variables
-     &    MaxFiles = 100,   !Maximum number of output files
+     &    MaxFiles = 200,   !Maximum number of output files
      &    MaxPest = 500    !Maximum number of pest operations
 
       REAL, PARAMETER :: 
