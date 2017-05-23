c3bf6b4c3391d7688f60688e912c620c4e0cfe67
Cheryl Porter <cporter@ufl.edu>
2017-05-23 16:30:21 -0400

    Kick the Y2K can to 2030

--- DATES.for
+++ DATES.for
@@ -74,10 +74,10 @@ C=======================================================================
         YR  = INT(YRDOY / 1000)
         DOY = YRDOY - YR * 1000
         IF (YRDOY .GT. 0) THEN
-!         IF (YR .LE. 10) THEN
 !     CHP 09/11/2009 - change "cross-over" year from 2010 to 2015
 !     CHP 03/26/2014 - change "cross-over" year from 2015 to 2020
-          IF (YR .LE. 20) THEN
+!     CHP 05/23/2017 - change "cross-over" year from 2020 to 2030
+          IF (YR .LE. 30) THEN
             YRDOY = (2000 + YR) * 1000 + DOY
           ELSE
             YRDOY = (1900 + YR) * 1000 + DOY
