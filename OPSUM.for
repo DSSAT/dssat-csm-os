19e9d7c0ea875611ab0314f1c7c4babbfe5635b2
Cheryl Porter <cporter@ufl.edu>
2017-05-23 11:12:38 -0400

    Update csv routines for new summary.out variables

--- OPSUM.for
+++ OPSUM.for
@@ -501,7 +501,7 @@ C-------------------------------------------------------------------
           WRITE(NOUTDS,310)
   310     FORMAT(/,
      &'!IDENTIFIERS......................... ',
-     &'EXPERIMENT AND TREATMENT.............. ', 
+     &'EXPERIMENT AND TREATMENT.......... ', 
      &'SITE INFORMATION............ ',
      &'DATES..........................................  ',
      &'DRY WEIGHT, YIELD AND YIELD COMPONENTS....................',
@@ -518,7 +518,7 @@ C-------------------------------------------------------------------
 
           WRITE (NOUTDS,400)
   400     FORMAT ('@   RUNNO   TRNO R# O# C# CR MODEL... ',
-     &    'EXNAME...... TNAM..................... ',
+     &    'EXNAME.. TNAM..................... ',
      &    'FNAM.... WSTA.... SOIL_ID...  ',
      &    '  SDAT    PDAT    EDAT    ADAT    MDAT    HDAT',
      &    '  DWAP    CWAM    HWAM    HWAH    BWAH  PWAM',
@@ -543,7 +543,7 @@ C-------------------------------------------------------------------
         IF (FMOPT == 'A' .OR. FMOPT == ' ') THEN   ! VSH
         WRITE (NOUTDS,500,ADVANCE='NO') 
      &    RUN, TRTNUM, ROTNO, ROTOPT, CRPNO, 
-     &    CROP, MODEL, CONTROL%FILEX, TITLET, FLDNAM, WSTAT, SLNO,
+     &    CROP, MODEL, CONTROL%FILEX(1:8), TITLET, FLDNAM, WSTAT, SLNO,
      &    YRSIM, YRPLT, EDAT, ADAT, MDAT, YRDOY, 
      &    DWAP, CWAM, HWAM, NINT(HWAH), NINT(BWAH*10.), PWAM
 
@@ -658,12 +658,12 @@ C-------------------------------------------------------------------
         IF (FMOPT == 'C') THEN
             
             CALL CsvOutSumOpsum(RUN, TRTNUM, ROTNO, ROTOPT, CRPNO, CROP,
-     &MODEL, TITLET, FLDNAM, WSTAT, SLNO, YRSIM, YRPLT, EDAT, ADAT, 
-     &MDAT, YRDOY, DWAP, CWAM, HWAM, HWAH, BWAH, PWAM, 
-     &HWUM, HNUMUM, HIAM, LAIX, HNUMAM, IRNUM, IRCM, PRCM, ETCM,
+     &MODEL, CONTROL%FILEX(1:8), TITLET, FLDNAM, WSTAT, SLNO, YRSIM, 
+     &YRPLT, EDAT, ADAT, MDAT, YRDOY, DWAP, CWAM, HWAM, HWAH, BWAH, 
+     &PWAM, HWUM, HNUMUM, HIAM, LAIX, HNUMAM, IRNUM, IRCM, PRCM, ETCM,
      &EPCM, ESCM, ROCM, DRCM, SWXM, NINUMM, NICM, NFXM, NUCM, NLCM, 
-     &NIAM, CNAM, GNAM, PINUMM, PICM, PUPC, SPAM, KINUMM, KICM, KUPC, 
-     &SKAM, RECM, ONTAM, ONAM, OPTAM, OPAM, OCTAM, OCAM, 
+     &NIAM, CNAM, GNAM, N2OEC, PINUMM, PICM, PUPC, SPAM, KINUMM, KICM, 
+     &KUPC, SKAM, RECM, ONTAM, ONAM, OPTAM, OPAM, OCTAM, OCAM, CO2EC, 
      &DMPPM, DMPEM, DMPTM, DMPIM, YPPM, YPEM, YPTM, YPIM, DPNAM, 
      &DPNUM, YPNAM, YPNUM, NDCH, TMAXA, TMINA, SRADA, DAYLA, CO2A, 
      &PRCP, ETCP, ESCP, EPCP,   
