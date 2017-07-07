a9b019dc0ae8f98c7869c3f20cef20c5f7a92ab5
Willingthon Pavan <pavan@upf.br>
2017-07-07 17:49:42 -0400

    Cross-platform changes and Rebase from DSSAT Develop branch

--- SPSUBS.for
+++ SPSUBS.for
@@ -140,8 +140,9 @@ C-----------------------------------------------------------------------
                FRMT = '('//Trim(Adjustl(FRMT))//'(4X,A2,I1,A1),A8)'
                WRITE (LUN,FRMT) ("ES",L,"D",L=1,N_LYR), 'TRWUD' 
             ELSE
-              WRITE (LUN,122)("ES",L,"D",L=1,9), "        ES10D    RWUD"
-  122         FORMAT(9("    ",A2,I1,A1),A25)
+!              WRITE (LUN,122)("ES",L,"D",L=1,9, "        ES10D    RWUD")
+              WRITE (LUN,122)("ES",L,"D",L=1,9), "  ES10D   TRWUD"  !VSH
+  122         FORMAT(9("    ",A2,I1,A1),A16)
             ENDIF
             END IF   ! VSH
           ELSE
