13fefb0c87f273a9fc6e64fb4936ff848bd23cb7
Cheryl Porter <cporter@ufl.edu>
2017-08-14 20:54:18 -0400

    Fix problem with SoilTemp.OUT printing to a fort.47 file

--- OpStemp.for
+++ OpStemp.for
@@ -58,21 +58,21 @@ C=======================================================================
       RUN     = CONTROL % RUN
 
       IF (FMOPT == 'A' .OR. FMOPT == ' ') THEN   ! VSH
-      CALL GETLUN('OUTT',NOUTDT)
-!     Open the output files
-      OUTT = 'SoilTemp.OUT'
-      INQUIRE (FILE = OUTT, EXIST = FEXIST)
-      IF (FEXIST) THEN
-        OPEN (UNIT=NOUTDT, FILE=OUTT, STATUS='OLD',
-     &    IOSTAT = ERRNUM, POSITION='APPEND')
-        !IF (RNMODE .NE. 'Q') THEN
-        !ENDIF
-      ELSE
-        OPEN (UNIT=NOUTDT, FILE=OUTT, STATUS='NEW',
-     &    IOSTAT = ERRNUM)
- !      Write headers info to daily output file
-        WRITE(NOUTDT,'("*SOIL TEMPERATURE OUTPUT FILE (DAILY)")')
-      ENDIF
+        CALL GETLUN('OUTT',NOUTDT)
+!       Open the output files
+        OUTT = 'SoilTemp.OUT'
+        INQUIRE (FILE = OUTT, EXIST = FEXIST)
+        IF (FEXIST) THEN
+          OPEN (UNIT=NOUTDT, FILE=OUTT, STATUS='OLD',
+     &      IOSTAT = ERRNUM, POSITION='APPEND')
+          !IF (RNMODE .NE. 'Q') THEN
+          !ENDIF
+        ELSE
+          OPEN (UNIT=NOUTDT, FILE=OUTT, STATUS='NEW',
+     &      IOSTAT = ERRNUM)
+ !        Write headers info to daily output file
+          WRITE(NOUTDT,'("*SOIL TEMPERATURE OUTPUT FILE (DAILY)")')
+        ENDIF
       END IF   ! VSH
 C-----------------------------------------------------------------------
 C     Variable heading for SoilTemp.OUT
@@ -80,32 +80,32 @@ C-----------------------------------------------------------------------
       IF (RNMODE .NE. 'Q' .OR. RUN .EQ. 1) THEN
 
         IF (FMOPT == 'A' .OR. FMOPT == ' ') THEN   ! VSH
-        !For first run of a sequenced run, use replicate
-        ! number instead of run number in header.
-        IF (RNMODE .EQ. 'Q') THEN
-          CALL HEADER(SEASINIT, NOUTDT, REPNO)
-        ELSE
-          CALL HEADER(SEASINIT, NOUTDT, RUN)
-        ENDIF
+          !For first run of a sequenced run, use replicate
+          ! number instead of run number in header.
+          IF (RNMODE .EQ. 'Q') THEN
+            CALL HEADER(SEASINIT, NOUTDT, REPNO)
+          ELSE
+            CALL HEADER(SEASINIT, NOUTDT, RUN)
+          ENDIF
         END IF   ! VSH
-        
+          
         CALL GET(SOILPROP)
         N_LYR = MIN(10, MAX(4,SOILPROP%NLAYR))
-
+          
         IF (FMOPT == 'A' .OR. FMOPT == ' ') THEN   ! VSH
-        WRITE (NOUTDT, '("! TAV  =",F8.1,/,"! TAMP =",F8.1)') TAV, TAMP
-        WRITE (NOUTDT, 
-     &    '("!",T17,"Temperature (oC) by soil depth (cm):",
-     &    /,"!",T17,"Surface",10A8)') (SoilProp%LayerText(L), L=1,N_LYR)
-        IF (N_LYR < 10) THEN
-          WRITE (NOUTDT,120) ("TS",L,"D",L=1,N_LYR)
-  120     FORMAT('@YEAR DOY   DAS    TS0D',10("    ",A2,I1,A1))
-!     &  '    TS1D    TS2D    TS3D    TS4D    TS5D',
-!     &  '    TS6D    TS7D    TS8D    TS9D    TS10')
-        ELSE
-          WRITE (NOUTDT,122) ("TS",L,"D",L=1,9), "    TS10"
-  122     FORMAT('@YEAR DOY   DAS    TS0D',9("    ",A2,I1,A1),A8)
-        ENDIF
+          WRITE (NOUTDT, '("! TAV  =",F8.1,/,"! TAMP =",F8.1)') TAV,TAMP
+          WRITE (NOUTDT, 
+     &      '("!",T17,"Temperature (oC) by soil depth (cm):",
+     &      /,"!",T17,"Surface",10A8)')(SoilProp%LayerText(L),L=1,N_LYR)
+          IF (N_LYR < 10) THEN
+            WRITE (NOUTDT,120) ("TS",L,"D",L=1,N_LYR)
+  120       FORMAT('@YEAR DOY   DAS    TS0D',10("    ",A2,I1,A1))
+!     &    '    TS1D    TS2D    TS3D    TS4D    TS5D',
+!     &    '    TS6D    TS7D    TS8D    TS9D    TS10')
+          ELSE
+            WRITE (NOUTDT,122) ("TS",L,"D",L=1,9), "    TS10"
+  122       FORMAT('@YEAR DOY   DAS    TS0D',9("    ",A2,I1,A1),A8)
+          ENDIF
         END IF   ! VSH
       ENDIF
 
@@ -133,19 +133,19 @@ C-----------------------------------------------------------------------
       IF (DOPRINT) THEN
         CALL YR_DOY(YRDOY, YEAR, DOY)
         IF (FMOPT == 'A' .OR. FMOPT == ' ') THEN   ! VSH
-C       Generate output for file SoilTemp.OUT
-        WRITE (NOUTDT,300) YEAR, DOY, DAS, SRFTEMP, (ST(L),L=1,N_LYR)
-  300   FORMAT(1X,I4,1X,I3.3,1X,I5,11F8.1)
+!         Generate output for file SoilTemp.OUT
+          WRITE (NOUTDT,300) YEAR, DOY, DAS, SRFTEMP, (ST(L),L=1,N_LYR)
+  300     FORMAT(1X,I4,1X,I3.3,1X,I5,11F8.1)
         END IF   ! VSH
 
-!     VSH CSV output corresponding to SoilTEMP.OUT
-      IF (FMOPT == 'C') THEN ! VSH
-         CALL CsvOutTemp_crgro(EXPNAME,CONTROL%RUN, CONTROL%TRTNUM,
+!       VSH CSV output corresponding to SoilTEMP.OUT
+        IF (FMOPT == 'C') THEN ! VSH
+          CALL CsvOutTemp_crgro(EXPNAME,CONTROL%RUN, CONTROL%TRTNUM,
      &CONTROL%ROTNUM,CONTROL%REPNO, YEAR, DOY, DAS, SRFTEMP,
      &N_LYR, ST, vCsvlineTemp, vpCsvlineTemp, vlngthTemp)
      
-         CALL LinklstTemp(vCsvlineTemp)
-      END IF
+          CALL LinklstTemp(vCsvlineTemp)
+        ENDIF
       
       ENDIF
 
