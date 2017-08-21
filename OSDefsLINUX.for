b929eaa715d9ec5c3dca31a89470c2702f4ba6be
Willingthon Pavan <pavan@upf.br>
2017-08-21 11:17:11 -0400

    Changed "Windows" to "Linux", fixing the documentation. As v4.7, STDPATH set to user home (~/DSSAT46/).

--- OSDefsLINUX.for
+++ OSDefsLINUX.for
@@ -4,7 +4,7 @@
 !=======================================================================
 
       MODULE OSDefinitions
-!     Contains defintion for Windows Platform which are used throughout 
+!     Contains defintion for Linux Platform which are used throughout 
 !     the model.
 
       SAVE
@@ -14,7 +14,7 @@
       CHARACTER(LEN=1), PARAMETER ::  SLASH = '/'
       character(len=3), PARAMETER ::  exe_string = '.so'
       CHARACTER(LEN=12), PARAMETER :: DSSATPRO = 'DSSATPRO.L46'
-      CHARACTER(LEN=11), PARAMETER :: STDPATH = '/DSSAT46/'
+      CHARACTER(LEN=11), PARAMETER :: STDPATH = '~/DSSAT46/'
 
 !======================================================================
       END MODULE OSDefinitions
