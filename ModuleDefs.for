45d509c13d12a7cbd1808430775b1eb9a5d5f7e1
Cheryl Porter <cporter@ufl.edu>
2017-10-20 10:47:59 -0400

    v4.7.0.4 branding

--- ModuleDefs.for
+++ ModuleDefs.for
@@ -97,13 +97,14 @@ C             CHP Added TRTNUM to CONTROL variable.
         INTEGER :: Major = 4
         INTEGER :: Minor = 7
         INTEGER :: Model = 0
-        INTEGER :: Build = 3
+        INTEGER :: Build = 4
       END TYPE VersionType
       TYPE (VersionType) Version
-      CHARACTER(len=10) :: VBranch = '-develop  '
-!     CHARACTER(len=10) :: VBranch = '-release  '
+!     CHARACTER(len=10) :: VBranch = '-develop  '
+      CHARACTER(len=10) :: VBranch = '-release  '
 
 !     Version history:  
+!       4.7.0.4  chp 10/18/2017 Updates to Aloha, N2O, YCA, potato
 !       4.7.0.3  chp 10/10/2017 N2O emissions model added
 !       4.7.0.2  chp 09/27/2017 Merge v4.7 branch into develop
 !       4.7.0.1  chp 09/13/2017 Add Aloha-Pineapple model
