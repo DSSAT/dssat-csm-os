15683d845080d17b2ff59a0a1a7e56a915cdac60
Cheryl Porter <cporter@ufl.edu>
2018-05-08 12:23:17 -0400

    v4.7.2.0

--- Utilities/ModuleDefs.for
+++ Utilities/ModuleDefs.for
@@ -47,31 +47,22 @@ C             CHP Added TRTNUM to CONTROL variable.
       TYPE VersionType
         INTEGER :: Major = 4
         INTEGER :: Minor = 7
-        INTEGER :: Model = 1
-        INTEGER :: Build = 6
+        INTEGER :: Model = 2
+        INTEGER :: Build = 0
       END TYPE VersionType
       TYPE (VersionType) Version
-      CHARACTER(len=10) :: VBranch = '-develop  '
-!     CHARACTER(len=10) :: VBranch = '-release  '
+!     CHARACTER(len=10) :: VBranch = '-develop  '
+      CHARACTER(len=10) :: VBranch = '-release  '
 
 !     Version history:  
-!       4.7.1.6  chp 05/02/2018 Canegro Jones & Singels, 2018; pineapple data files
-!       4.7.1.5  chp 02/16/2018 forage and csv mods
-!       4.7.1.4  chp 01/16/2018 YCA update, data file updates
-!       4.7.1.3  chp 11/30/2017 Tiledrain N loss, Hanseok Jeong
-!       4.7.1.2  chp 11/21/2017 Sugarbeet model added
-!       4.7.1.1  chp 10/31/2017 Pineapple forcing with chemical application
-!       4.7.1.0  chp 10/27/2017 Modifications to data, tweaks to output
-!       4.7.0.4  chp 10/18/2017 Updates to Aloha, N2O, YCA, potato
-!       4.7.0.3  chp 10/10/2017 N2O emissions model added
-!       4.7.0.2  chp 09/27/2017 Merge v4.7 branch into develop
-!       4.7.0.1  chp 09/13/2017 Add Aloha-Pineapple model
-!       4.7.0.0  chp 08/09/2017 v4.7
-!       4.6.5.1  chp 05/10/2017 Workshop 2017 version.  
-!       4.6.0.1  chp 06/28/2011 v4.6
-!       4.5.1.0  chp 10/10/2010 V4.5 Release version
-!       4.0.2.0  chp 08/11/2005 Release
-!       4.0.1.0  chp 01/28/2004 Release Version 
+!       4.7.2.0  chp 05/07/2018 v4.7.2 Release 2018 Workshop
+!       4.7.1.0  chp 10/27/2017 v4.7.1 Release
+!       4.7.0.0  chp 08/09/2017 v4.7.0 Release
+!       4.6.5.1  chp 05/10/2017 v4.6.5 Release 2017 Workshop  
+!       4.6.0.1  chp 06/28/2011 v4.6.0 Release
+!       4.5.1.0  chp 10/10/2010 V4.5.1 Release
+!       4.0.2.0  chp 08/11/2005 v4.0.2 Release
+!       4.0.1.0  chp 01/28/2004 v4.0.1 Release 
 
 !=======================================================================
 
