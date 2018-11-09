b047744a9045a74781d8a803c0a7393cdb62a4b1
Cheryl Porter <cporter@ufl.edu>
2018-11-09 13:44:29 -0500

    Add back in CSMVersion.for because ...

--- Utilities/CSMVersion.for
+++ Utilities/CSMVersion.for
@@ -14,13 +14,18 @@
         INTEGER :: Major = 4
         INTEGER :: Minor = 7
         INTEGER :: Model = 2
-        INTEGER :: Build = 2
+        INTEGER :: Build = 3
       END TYPE VersionType
       TYPE (VersionType) Version
-      CHARACTER(len=*), PARAMETER :: VBranch = '-develop'
-!     GitHash: 6224d9b6e7461b751ae857eabc73772dd21bf9e6
+      CHARACTER(len=*), PARAMETER :: VBranch = '-ASCE_kc_chp'
+!     Implement Kelly Thorp's ASCE PET routines for soybean
+!     GitHash: 6928dc31442e29f6c1b00f6f612818362b57a951
 
 !     Version history:  
+!       4.7.2.3  chp 10/25/2018 Rice - G5 parameter   
+!       4.7.2.2  chp 10/25/2018 Sugarbeet, Forage, Cassava,    
+!                               ASCE ET added (on hold),
+!                               Fixed bug with maize byproduct harvest percentage 
 !       4.7.2.1  chp 07/08/2018 Use CSMVersion, minor bug fixes, Linux hacks, 
 !                                 VSCode files
 !       4.7.2.0  chp 05/07/2018 v4.7.2 Release 2018 Workshop
