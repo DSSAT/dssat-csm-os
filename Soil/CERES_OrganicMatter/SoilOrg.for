2421a7f7dd93b9a8648130f1f50def5f6787c5b2
MengZhang <meng.zhang15@ufl.edu>
2020-05-26 12:51:01 -0400

    Fix an issue which might cause exception because 2D change NL from 20 to 30

--- Soil/CERES_OrganicMatter/SoilOrg.for
+++ Soil/CERES_OrganicMatter/SoilOrg.for
@@ -890,7 +890,7 @@ C     Write seasonal output
      &    FON, HARVRES, IMM, LITE, MNR, MULCH, N_ELEMS,   !Input
      &    NLAYR, OMADATA, SENESCE, TLITE, TSOME)          !Input
 
-      write(777,'(i8,40f8.2)')
+      write(777,'(i8,60f8.2)')
      &   yrdoy, cn_som(1:nlayr), cn_fom(1:nlayr)
 
 C***********************************************************************
