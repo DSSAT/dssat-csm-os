adb94ec0dd993708f5028364813d80d782a9bbb9
Patricia Moreno <lpmorenoc@ufl.edu>
2021-01-04 11:45:44 -0500

    Define excess of N just for the leaves that are not growing

--- Plant/CSYCA-Cassava/YCA_Integ_N.f90
+++ Plant/CSYCA-Cassava/YCA_Integ_N.f90
@@ -41,16 +41,14 @@
         LEAFN = 0.0
         DO BR = 0, BRSTAGE                                                                                        !LPM23MAY2015 To consider different N demand by node according with its age                                                                       
             DO LF = 1, LNUMSIMSTG(BR)
+                !LPM Define excess of N for the leaves that are not growing
                 IF (isLeafAlive(node(BR,LF))) THEN
-                    IF (node(BR,LF)%LANC > node(BR,LF)%LNCX) THEN 
+                    IF (node(BR,LF)%LANC > node(BR,LF)%LNCX .AND. .NOT. isLeafExpanding(node(BR,LF))) THEN 
                         node(BR,LF)%LEAFNEXCESSN = ((leafAreaLeftToSenesce(node(BR,LF))/LAWL(1)) / (1.0-LPEFR))*(node(BR,LF)%LANC-node(BR,LF)%LNCX)
                         LEAFNEXCESS = LEAFNEXCESS + node(BR,LF)%LEAFNEXCESSN
                     ENDIF
                     !LPM 01DEC2020 Assume harvesting proportional for all the leaves that are active (LNPH) and N increase just for the leaves that are growing 
-                    node(BR,LF)%LEAFNN = node(BR,LF)%LEAFNN  + LNUSEN(1,BR,LF)+ LNUSEN(2,BR,LF) - node(BR,LF)%SENNLFGN - node(BR,LF)%SENNLFGRSN - (LNPH/Lcounth) - node(BR,LF)%LEAFNEXCESSN 
-                    IF (isLeafExpanding(node(BR,LF))) THEN
-                       node(BR,LF)%LEAFNN = node(BR,LF)%LEAFNN
-                    ENDIF
+                    node(BR,LF)%LEAFNN = AMIN1(0.0,node(BR,LF)%LEAFNN  + LNUSEN(1,BR,LF)+ LNUSEN(2,BR,LF) - node(BR,LF)%SENNLFGN - node(BR,LF)%SENNLFGRSN - (LNPH/Lcounth) - node(BR,LF)%LEAFNEXCESSN) 
                     LEAFN= LEAFN + node(BR,LF)%LEAFNN
                 ENDIF
             ENDDO
