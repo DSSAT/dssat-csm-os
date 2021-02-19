c63ad193fb1b6d3844158233963266e9f0db4cca
chporter <cporter@ufl.edu>
2021-02-19 13:09:27 -0500

    Handle short data period in Long weather file.

--- Weather/IPWTH_alt.for
+++ Weather/IPWTH_alt.for
@@ -219,7 +219,11 @@ C     The components are copied into local variables for use here.
       NYEAR = (ICHAR(FILEW(7:7)) - 48)*10 + (ICHAR(FILEW(8:8)) - 48)
       IF (LastWeatherDay > FirstWeatherDay) THEN
         NYEAR = INT(LastWeatherDay)/1000 - INT(FirstWeatherDay)/1000 + 1
-        NYEAR = MAX(1, NYEAR)
+        IF (LongFile) THEN
+          NYEAR = MAX(2, NYEAR)
+        ELSE  
+          NYEAR = MAX(1, NYEAR)
+        ENDIF
       ENDIF
 
 !     Detect name of weather file based on MEWTH
