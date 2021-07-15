ce2b8cec02181cc401268f84792f756be7d545f0
Patricia Moreno <lpmorenoc@ufl.edu>
2021-07-15 09:34:12 -0400

    Include petiole weight in leaf weight output

--- Plant/CSYCA-Cassava/YCA_Control_Plant.f90
+++ Plant/CSYCA-Cassava/YCA_Control_Plant.f90
@@ -28,7 +28,7 @@
     real function canopyWeight()
         implicit none
         
-        canopyWeight = woodyWeight()+(LFWT*(1.0-LPEFR))
+        canopyWeight = woodyWeight()+LFWT
     end function canopyWeight
     
     ! real value of the total weight of the plant: leaves, stem, crown, reserves and root storage organ
