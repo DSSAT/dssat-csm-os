a09659369aedb989e748fc41a7048c8573cc4528
Sohail Khan <sohail.wur@gmail.com>
2023-10-26 08:05:35 +0200

    Fix the DTT formatting

--- Plant/SUBSTOR-Potato/PT_OPGROW.for
+++ Plant/SUBSTOR-Potato/PT_OPGROW.for
@@ -84,25 +84,25 @@ C=======================================================================
 C-----------------------------------------------------------------------
       DATA GROHEAD /
 !      DATA GROHEAD(1)/
-     &'! YR       Days  Thermal Days  Grow       Fresh          
+     &'! YR      Thermal  Days   Days  Grow       Fresh
      &      Dry Weight                           Pod      Phot. Grow    
      &   Leaf Shell   Spec    Canopy          Root  ³    Root Length Den
      &sity   ³ Senesced mass              ',
 
 !      DATA GROHEAD(2)/
-     &'!   and   after Time after Stage  LAI  Yield  Leaf  St
+     &'!   and   Time     after  after Stage  LAI  Yield  Leaf  St
      &em Tuber  Root  Crop  Tops DLeaf   HI   Wgt.   No.    Water     Ni
      &t.   Nit -ing   Leaf  Hght  Brdth      Depth  ³     cm3/cm3   of 
      &soil    ³    (kg/ha)                ',
 
 !      DATA GROHEAD(3)/
-     &'!     DOY   sim      plant             Mg/Ha  ³<------
+     &'!     DOY          sim     plant             Mg/Ha  ³<------
      &--------- kg/Ha --------------->³      Kg/Ha        ³<Stress (0-1)
      &>³    %     %   Area    m     m           m   ³<------------------
      &------>³ Surface  Soil              ',
 
 !      DATA GROHEAD(4) / 
-     &'@YEAR DOY   DAS  DTT  DAP  GSTD  LAID  UYAD  LWAD  
+     &'@YEAR DOY  DTT     DAS    DAP   GSTD  LAID  UYAD  LWAD
      &SWAD  UWAD  RWAD  TWAD  CWAD  DWAD  HIAD  EWAD  E#AD  WSPD  WSGD  
      &NSTD  LN%D  SH%D  SLAD  CHTD  CWID  EWSD  RDPD  RL1D  RL2D  RL3D  
 !     &RL4D  RL5D              '/
@@ -327,7 +327,7 @@ C
 
 !       PlantGro.out file
         IF (IDETG .EQ. 'Y') THEN
-          WRITE (NOUTDG,400)YEAR, DOY, DAS,DTT,DAP,RSTAGE,XLAI,FRYLD,
+          WRITE (NOUTDG,400)YEAR,DOY,DTT,DAS,DAP,RSTAGE,XLAI,FRYLD,
      &        NINT(WTLF*10.0),NINT(STMWT*GM2KG),NINT(SDWT*GM2KG),
      &        NINT(RTWT*GM2KG),NINT(BIOMAS*10.0),
      &        NINT(WTLF*10.0)+NINT(STMWT*GM2KG),NINT(DEADLF*GM2KG),HI,
@@ -335,7 +335,7 @@ C
      &        1.0-NSTRES,PCNL,SHELPC,SLA,CANHT,CANWH,SATFAC,
      &        (RTDEP/100),(RLV(I),I=1,5)
      &       ,NINT(CUMSENSURF), NINT(CUMSENSOIL)
- 400      FORMAT (1X,I4,1X,F2.2,I3.3,3(1X,I5),1X,F5.2,1X,F5.1,7(1X,I5),
+ 400      FORMAT (1X,I4,1X,I3.3,2X,F2.2,1X,3(1X,I5),1X,F5.2,1X,F5.1,7(1X,I5),
      &          1X,F5.3,2(1X,I5),3(1X,F5.3),2(1X,F5.2),1X,F5.1,
      &          2(1X,F5.2),1X,F5.3,6(1X,F5.2), 2I6)
         ENDIF
