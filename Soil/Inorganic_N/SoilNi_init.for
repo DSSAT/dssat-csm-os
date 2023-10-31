831f50b440d49a0fe745033038eeeb464af0d3be
chporter <cherylhporter@gmail.com>
2023-10-31 12:09:46 +1000

    integration of soil N initialization now done for 1D and 2D in one file. - remove the 2D file

--- Soil/Inorganic_N/SoilNi_init.for
+++ Soil/Inorganic_N/SoilNi_init.for
@@ -1,138 +1,111 @@
-C***********************************************************************
-C  SoilNi_init, Subroutine
-C
-C  Purpose: Do soil N initialization.
-C  This was modified from SOILNI when NTRANS was split into organic and 
-C  inorganic sections.  SOILNI was split into SOILNI_inorganic and 
-C  SOILNI_organic.
-C-----------------------------------------------------------------------
-C  REVISION HISTORY 
-C  02/08/1993 PWW Header revision and minor changes.
-C  02/20/1996 GH  Written.
-C  02/26/1998 WTB Fixed HUMC/HUMN calculations.
-C  06/09/1999 AJG Completely revised the soil N and SOM module, and made
-C               a new SOM module based on the CENTURY model.
-C               Also changed variable names:
-C         OLD         NEW       
-C         -------     -------   
-C         TFY         TFNITY          
-C         WRESR       ICRT, TRTRES
-C  06/21/1999 CHP Modular format
-C  03/16/2000 GH  Incorporated in CROPGRO
-C  06/11/2002 GH  Modified for Y2K
-C  08/12/2003 CHP Added I/O error checking
-C                 No re-initialization done for sequenced runs
+!***********************************************************************
+!  SoilNi_init, Subroutine
+!
+!  Purpose: Do soil N initialization for 1D layers and 2D cells.
+!  This was modified from SOILNI when NTRANS was split into organic and 
+!    inorganic sections.  SOILNI was split into SOILNI_inorganic and 
+!    SOILNI_organic.
+!  Input layer depths for initial NO3 and NH4 are adjusted for bed
+!     construction.
+!-----------------------------------------------------------------------
+!  REVISION HISTORY 
+!  02/08/1993 PWW Header revision and minor changes.
+!  02/20/1996 GH  Written.
+!  02/26/1998 WTB Fixed HUMC/HUMN calculations.
+!  06/09/1999 AJG Completely revised the soil N and SOM module, and made
+!               a new SOM module based on the CENTURY model.
+!  06/21/1999 CHP Modular format
+!  03/16/2000 GH  Incorporated in CROPGRO
+!  06/11/2002 GH  Modified for Y2K
+!  08/12/2003 CHP Added I/O error checking
+!                 No re-initialization done for sequenced runs
 !  01/14/2005 CHP/UPS Split NTRANS into separate organic and 
 !                  inorganic routines.
-C-----------------------------------------------------------------------
-C  Called : SOILN_inorg
-C  Calls  : ERROR, FIND
-C=======================================================================
+!  06/01/2010 CHP / JZW Modified for 2D model initialization
+!  08/15/2011  Move the NH4I and NO3I reading and handling from this subroutine to CellInit_2D
+!                because the initial condition in SoilCNPinit_C is called by Century and
+!                SoilNi_init_2D is after the call of Century
+!              Add NH4 and No3 to the argument of this subroutine
+!  10/31/2023 CHP integrated 1D and 2D versions.
+!=======================================================================
 
-      SUBROUTINE SoilNi_init(CONTROL, ISWNIT,
-     &    SOILPROP, ST,                                   !Input
-     &    NH4, NO3, SNH4, SNO3, TFNITY, UPPM, UREA)       !Output
+      SUBROUTINE SoilNi_init(CONTROL, 
+     &    Cell_Type, SOILPROP, ST, NH4, NO3,      !Input
+     &    NH4_2D, NO3_2D, SNH4, SNH4_2D, SNO3,    !Output
+     &    SNO3_2D, TFNITY, UPPM, UREA, UREA_2D)   !Output
       
 !-----------------------------------------------------------------------
-      USE ModuleDefs     !Definitions of constructed variable types, 
-                         ! which contain control information, soil
-                         ! parameters, hourly weather data.
+      USE Cells_2D
       IMPLICIT  NONE
-      EXTERNAL ERROR, FIND
       SAVE
 
-      CHARACTER*1 RNMODE, ISWNIT
-      CHARACTER*6 ERRKEY, SECTION, DUMMY
-      CHARACTER*30 FILEIO
-      PARAMETER (ERRKEY = 'SOILNI')
+!     2D variables
+      INTEGER, DIMENSION(MaxRows,MaxCols) :: Cell_Type
+      REAL, DIMENSION(MaxRows,MaxCols) :: ColFrac
+      REAL, DIMENSION(MaxRows,MaxCols) :: NH4_2D, NO3_2D, TFNITY
+      REAL, DIMENSION(MaxRows,MaxCols) :: SNH4_2D, SNO3_2D, UREA_2D
 
-      INTEGER ERRNUM, FOUND, L, LNUM
-      INTEGER LUNIO, NLAYR
+      CHARACTER*1 RNMODE 
+
+      INTEGER L,J  !ERRNUM, FOUND, LNUM
       INTEGER RUN
 
       REAL KG2PPM(NL)
       REAL NH4(NL), NO3(NL)
-      REAL SNH4(NL)
-      REAL SNO3(NL), ST(NL), TFNITY(NL) 
+      REAL SNH4(NL), SNO3(NL)
       REAL UREA(NL), UPPM(NL)
+      REAL ST(NL) 
 
 !-----------------------------------------------------------------------
-!     Define constructed variable types based on definitions in
-!     ModuleDefs.for.
-
 !     Constructed variables are defined in ModuleDefs.
       TYPE (ControlType) CONTROL
       TYPE (SoilType)    SOILPROP
 
 !     Transfer values from constructed data types into local variables.
-      FILEIO  = CONTROL % FILEIO
-      LUNIO   = CONTROL % LUNIO
       RNMODE  = CONTROL % RNMODE
       RUN     = CONTROL % RUN
-
       KG2PPM = SOILPROP % KG2PPM    
-      NLAYR  = SOILPROP % NLAYR  
+      ColFrac= BedDimension % ColFrac
+
+      NH4_2D = 0.0
+      NO3_2D = 0.0
+      UREA_2D = 0.0
+      UREA = 0.0
+      Uppm = 0.0
 
 !***********************************************************************
       IF (RUN .EQ. 1 .OR. INDEX('QF',RNMODE) .LE. 0) THEN
-        IF (INDEX(ISWNIT,'N') < 1) THEN
-!         --------------------------------------------------------------
-!         Open the FILEIO input file.
-          OPEN (LUNIO, FILE = FILEIO, STATUS = 'OLD', IOSTAT = ERRNUM)
-          IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY, ERRNUM, FILEIO, 0)
-!         --------------------------------------------------------------
-!         Find and Read INITIAL CONDITIONS Section.
-!         --------------------------------------------------------------
-          SECTION = '*INITI'
-          CALL FIND (LUNIO, SECTION, LNUM, FOUND)
-          IF (FOUND .EQ. 0) CALL ERROR (SECTION, 42, FILEIO, LNUM)
-        
-!         Read line before layer data 
-          READ (LUNIO, '(A6)', IOSTAT = ERRNUM) DUMMY
-          LNUM = LNUM + 1
-          IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY, ERRNUM, FILEIO, LNUM)
-        
-          DO L = 1, NLAYR
-            LNUM = LNUM + 1
-            READ(LUNIO, 100, IOSTAT=ERRNUM) NH4(L),NO3(L)
-100         FORMAT (14X, 2 (1X, F5.1))
-            IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY, ERRNUM, FILEIO, LNUM)
-          ENDDO
-          CLOSE (LUNIO)
-        ELSE
-!         Set some default concentrations, needed for ORYZA even when no N simulated
-          NO3 = 0.1
-          NH4 = 0.1
-          UPPM = 0.0
-        ENDIF
-
-!       !Non-sequenced runs
-        DO L = 1, NLAYR
+        DO L = 1, NRowsTot
 !         --------------------------------------------------------------
 !         Calculate yesterday's soil temperature factor. When
 !         calculating the nitrification, TFNITY will be compared with
 !         today's soil temperature factor, and the maximum will apply.
           IF (ST(L) .LT. 5.0) THEN
-            TFNITY(L) = 0.0
+            TFNITY(L,1) = 0.0
           ELSE
-            TFNITY(L) = 0.0009766 * ST(L) * ST(L)
+            TFNITY(L,1) = 0.0009766 * ST(L) * ST(L)
           ENDIF
-
-!         --------------------------------------------------------------
-!         Initialize soil mineral nitrogen and urea.
-!         --------------------------------------------------------------
-!         Limit NO3 and NH4 to >=0.01.
-!          NO3(L) = AMAX1 (NO3(L), 0.01)
-!          NH4(L) = AMAX1 (NH4(L), 0.01)
-          NO3(L) = AMAX1 (NO3(L), 1.E-6)
-          NH4(L) = AMAX1 (NH4(L), 1.E-6)
 
 !         Convert the N concentrations to kg[N] / ha per soil layer.
           SNO3(L) = NO3(L) / KG2PPM(L)
           SNH4(L) = NH4(L) / KG2PPM(L)
 
-!         Initialize urea.
-          UREA(L) = 0.0
+          DO J = 1, NColsTot
+            TFNITY(L,J) = TFNITY(L,1)
+!           --------------------------------------------------------------
+!           Initialize soil mineral nitrogen and urea.
+!           --------------------------------------------------------------
+            SELECT CASE (Cell_type(L,J))
+            CASE (3,4,5)
+              NO3_2D(L, J) = NO3(L)
+              NH4_2D(L, J) = NH4(L)
+
+!             Convert the N concentrations to kg[N] / ha per soil layer.
+              SNO3_2D(L, J) = SNO3(L) * ColFrac(L,J)
+              SNH4_2D(L, J) = SNH4(L) * ColFrac(L,J)
+
+            END SELECT
+          ENDDO
         END DO   !End of soil layer loop.
       ENDIF  !End of RUN if-construct
 
@@ -143,26 +116,22 @@ C=======================================================================
 !=======================================================================
 ! SOILNI Variables
 !
-! ERRKEY     Subroutine name for error file 
-! ERRNUM     Error number for input 
-! FILEIO     Filename for input file (e.g., IBSNAT35.INP) 
-! FOUND      Indicator that good data was read from file by subroutine FIND 
-!              (0 - End-of-file encountered, 1 - NAME was found) 
 ! KG2PPM(L)  Conversion factor to switch from kg [N] / ha to µg [N] / g 
 !              [soil] for soil layer L 
-! LNUM       Current line number of input file 
-! LUNIO      Logical unit number for FILEIO 
-! NH4(L)     Ammonium N in soil layer L (µg[N] / g[soil])
-! NL         Maximum number of soil layers = 20 
+! NH4(L)     1D Ammonium N in soil layer L (µg[N] / g[soil])
+! NH4_2D(L,J)2D Ammonium N in soil cell L,J (µg[N] / g[soil])
 ! NLAYR      Actual number of soil layers 
-! NO3(L)     Nitrate in soil layer L (µg[N] / g[soil])
+! NO3(L)     1D Nitrate in soil layer L (µg[N] / g[soil])
+! NO3_2D(L)  2D Nitrate in soil cell L,J (µg[N] / g[soil])
 ! SECTION    Section name in input file 
-! SNH4(L)    Total extractable ammonium N in soil layer L (kg [N] / ha)
-! SNO3(L)    Total extractable nitrate N in soil layer L (kg [N] / ha)
+! SNH4(L)    1D Total extractable ammonium N in soil layer L (kg [N] / ha)
+! SNH4_2D(L,J) Total extractable ammonium N in cell L,J (kg [N] / ha)
+! SNO3(L)    1D Total extractable nitrate N in soil layer L (kg [N] / ha)
+! SNO3_2D(L) 2D Total extractable nitrate N in cell L,J (kg [N] / ha)
 ! ST(L)      Soil temperature in soil layer L (°C)
 ! SW(L)      Volumetric soil water content in layer L
 !              (cm3 [water] / cm3 [soil])
-! TFNITY(L)  Yesterday’s soil temperature factor for nitrification (range 
-!              0-1) 
-! UREA(L)    Amount of urea in soil layer L (kg [N] / ha)
+! TFNITY(L)  Yesterday’s soil temperature factor for nitrification (range 0-1) 
+! UREA(L)    1D Amount of urea in soil layer L (kg [N] / ha)
+! UREA_2D(L,J) 2D Amount of urea in soil cell L,J (kg [N] / ha)
 !=======================================================================
