04d79d386c271c6977ef67e0f4d12612cf8e0427
chporter <cherylhporter@gmail.com>
2023-10-31 10:46:34 +1000

    Modify NCHECK_inorg_2D.for to handle messages for 1D case also (layer vs cell ID)

--- Soil/Inorganic_N/NCHECK_inorg_2D.for
+++ Soil/Inorganic_N/NCHECK_inorg_2D.for
@@ -10,56 +10,68 @@ C               Note: File names etc. should be dynamically created
 C               Check time stamp
 !  02/25/2005 CHP Split NCHECK into organic and inorganic.
 C-----------------------------------------------------------------------
-      SUBROUTINE NCHECK_inorg_2D(CONTROL, 
+      SUBROUTINE NCHECK_inorg_2D(CONTROL, Sim2D,
      &    NH4_2D, NO3_2D, SNH4_2D, SNO3_2D, UREA_2D) !Input  
-    !&    NLAYR, NH4, NO3, SNH4, SNO3, UREA)              !Input
 
 !-----------------------------------------------------------------------
       USE Cells_2D
-      USE ModuleDefs     !Definitions of constructed variable types, 
-                         ! which contain control information, soil
-                         ! parameters, hourly weather data.
+      USE ModuleDefs
       IMPLICIT NONE 
       EXTERNAL WARNING
 !-----------------------------------------------------------------------
-      CHARACTER*78  MSG(10)
-      INTEGER L, J, YRDOY !, NLAYR
-      !INTEGER L, NLAYR, YRDOY
-      REAL, DIMENSION(MaxRows, MaxCols) :: NH4_2D, NO3_2D, SNH4_2D, 
-     &   SNO3_2D, UREA_2D
-    ! REAL, DIMENSION(NL) :: NH4, NO3, SNH4, SNO3, UREA
-      TYPE (ControlType) CONTROL
-      YRDOY   = CONTROL % YRDOY
+      TYPE (ControlType), INTENT(IN) :: CONTROL
+      LOGICAL, INTENT(IN) :: Sim2D
+      REAL, DIMENSION(MaxRows, MaxCols), INTENT(INOUT) ::
+     &     NH4_2D, NO3_2D, SNH4_2D,SNO3_2D, UREA_2D
+
+      CHARACTER*78 MSG(10)
+      INTEGER L, J, YRDOY
+      REAL, PARAMETER :: TOL = -1.E-6
+
+      IF (CONTROL % DYNAMIC == SEASINIT) THEN
+        WRITE(MSG(3),"('Value will be set to zero')")
+      ENDIF
 
 !-----------------------------------------------------------------------
-      WRITE(MSG(3),"('Value will be set to zero')")
+      YRDOY   = CONTROL % YRDOY
 
 !     Check for negative soil N values
       DO L = 1, NRowsTot
         DO J = 1, NColsTot
-          WRITE(MSG(1),100) YRDOY, L, J
-  100     FORMAT('Negative soil N value on day ',I7,' in cell ',I3, I3)
+          IF (Sim2D) THEN
+            WRITE(MSG(1),100) YRDOY, L, J
+  100       FORMAT('Negative soil N value on day ',I7,' in cell ',I3,I3)
+          ELSE
+            WRITE(MSG(1),200) YRDOY, L
+  200       FORMAT('Negative soil N value on day ',I7,' in layer ',I3)
+          ENDIF
 
-          IF (SNO3_2D(L, J).LT. -0.1) THEN
-            WRITE(MSG(2),"('Nitrate (SNO3_2D) =',F10.3,'kg[N]/ha')")
+          IF (SNO3_2D(L, J).LT. 0.0) THEN
+            SNO3_2D(L,J) = 0.0
+            NO3_2D(L,J) = 0.0
+            IF (SNO3_2D(L,J) .LT. TOL) THEN
+              WRITE(MSG(2),"('Nitrate =',F10.3,'kg[N]/ha')")
      &                    SNO3_2D(L, J)
-            CALL WARNING(3, "NCHECK", MSG)
-            SNO3_2D(L, J) = 0.0
-            NO3_2D(L, J) = 0.0
+              CALL WARNING(3, "NCHECK", MSG)
+            ENDIF
           ENDIF
 
-          IF (SNH4_2D(L, J).LT. -0.1) THEN
-            WRITE(MSG(2),"('Ammonium (SNH4_2D) =',F10.3,'kg[N]/ha')")
+          IF (SNH4_2D(L, J).LT. 0.0) THEN
+            SNH4_2D(L,J) = 0.0
+            NH4_2D(L,J) = 0.0
+            IF (SNH4_2D(L,J) .LT. TOL) THEN
+              WRITE(MSG(2),"('Ammonium =',F10.3,'kg[N]/ha')")
      &                   SNH4_2D(L, J)
-            CALL WARNING(3, "NCHECK", MSG)
-            SNH4_2D(L, J) = 0.0
-            NH4_2D(L, J) = 0.0
+              CALL WARNING(3, "NCHECK", MSG)
+            ENDIF
           ENDIF
 
-          IF (UREA_2D(L, J).LT. -0.1) THEN
-            WRITE(MSG(2),"('UREA_2D =',F10.3,'kg[N]/ha')") UREA_2D(L, J)
-            CALL WARNING(3, "NCHECK", MSG)
-            UREA_2D(L, J) = 0.0
+          IF (UREA_2D(L, J).LT. 0.0) THEN
+            UREA_2D(L,J) = 0.0
+            IF (UREA_2D(L,J) .LT. TOL) THEN
+              WRITE(MSG(2),"('Urea =',F10.3,'kg[N]/ha')") UREA_2D(L, J)
+              CALL WARNING(3, "NCHECK", MSG)
+            ENDIF
           ENDIF
         ENDDO
       ENDDO
