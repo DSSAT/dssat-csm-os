3d962ff22528a9e8044383e34fc3270757593166
Sohail Khan <sohail.wur@gmail.com>
2023-11-11 15:52:17 +0100

    OPSTRESS.for: fix the crash due to wrong format

--- Utilities/OPSTRESS.for
+++ Utilities/OPSTRESS.for
@@ -323,9 +323,10 @@
 
         IF (RNMODE .EQ. 'I' .AND. NYRS .LE. 1) THEN
           IF (RAINR(I) < 1000.) THEN
-            WRITE(*,600) STAG(I), NNR(I), TMAXR(I), TMINR(I), 
+            WRITE(*,620) STAG(I), NNR(I), TMAXR(I), TMINR(I),
      &        RADR(I), DAYLR(I), RAINR(I), CETR(I), W_photR(I), 
      &        W_growR(I), N_photR(I), N_growR(I), P_photR(I), P_growR(I)
+  620       FORMAT(1X,A23,I5,4F6.1,F7.2,4F7.1,6F7.3)
           ELSE
             WRITE(*,610) STAG(I), NNR(I), TMAXR(I), TMINR(I), 
      &        RADR(I), DAYLR(I), NINT(RAINR(I)), NINT(CETR(I)),
