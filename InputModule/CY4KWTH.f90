96b6f3b53978e466a285ac362ad4da0992c9422e
Fabio Oliveira <fabioaugustooliveira72@gmail.com>
2025-07-16 18:43:14 -0400

    Updated and added function to create/check weather filename and path

--- InputModule/CY4KWTH.f90
+++ InputModule/CY4KWTH.f90
@@ -1,11 +1,4 @@
 !=======================================================================
-!  COPYRIGHT 1998-2020 DSSAT Foundation
-!                      University of Florida, Gainesville, Florida
-!                      Inernational Fertilizer Development Center
-!  
-!  ALL RIGHTS RESERVED
-!=======================================================================
-!=======================================================================
 !  MAKEFILEW, Subroutine, Fabio Olivera, Willingthon Pavan, Gerrit Hoogenboom
 !
 ! This subroutine open the FILEX and read the weather station characters (WSTA).
@@ -25,21 +18,22 @@
 !-----------------------------------------------------------------------
 !=======================================================================
 
-      SUBROUTINE MAKEFILEW(LUNEXP,DSSATP,PATHEX,FILEX,        &              
+      SUBROUTINE CHECK_Y4K_WTH(CONTROL,MEWTH,LUNEXP,DSSATP,PATHEX,FILEX, &              
                     SimLevel,LNSIM,LNPLT,LNFLD)  !Input: Factor Levels
 
       USE ModuleDefs
       IMPLICIT      NONE
-      EXTERNAL ERROR, WARNING, IGNORE2, GETLUN, FIND, HFIND, INFO, PATH
+      EXTERNAL ERROR, WARNING, IGNORE2, GETLUN, FIND, HFIND, INFO, PATH, &
+               UPCASE, WTHFDIR
 
-      CHARACTER*1   BLANK, WTHER
+      CHARACTER*1   BLANK, UPCASE, MEWTH, RNMODE
       CHARACTER*3   PROCOD
       CHARACTER*6   ERRKEY,SECTION
       CHARACTER*5   FINDH
-      CHARACTER*8   WSTA,FILEW4,FILEX
-      CHARACTER*12  FILEW,NAMEF, LastFileW
+      CHARACTER*8   WSTA,WSTA1,FILEW4
+      CHARACTER*12  NAMEF,LastFileW,FILEX,FILEW,FILEWG,FILEWC,FILEWTH
       CHARACTER*78  MSG(4)
-      CHARACTER*80  PATHWT,PATHEX,CHARTEST
+      CHARACTER*80  PATHEX,CHARTEST,PATHWT,PATHWTC,PATHWTG,PATHWTW
       CHARACTER*92  FILEWW,FILETMP
       CHARACTER*102 DSSATP
       CHARACTER*255 LINE
@@ -47,23 +41,27 @@
       LOGICAL       FEXIST,SimLevel
       
       INTEGER       LUNEXP,LINEXP,LNUM,ISECT,ERRNUM,PATHL,IFIND !,FOUND
-      INTEGER       ICASAF,UNIT  !,LINWTH,LUNWTH
+      INTEGER       ICASAF,UNIT, I  !,LINWTH,LUNWTH
       INTEGER       SDATE,PDATE,YRDOY
       INTEGER       LNFLD,TLNFLD
       INTEGER       LNSIM,TLNSIM
       INTEGER       LNPLT,TLNPLT
-      INTEGER       YEARDOY,YEAR,YR,DOY
+      INTEGER       YEARDOY,YEAR,YR,DOY,YRSIM
       
       DATA LastFileW /" "/
 
-      PARAMETER (ERRKEY = 'MAKEFW')
+      PARAMETER (ERRKEY = 'Y4KWTH')
       PARAMETER (BLANK = ' ')
       
-      
-      SDATE = -99
-      PDATE = -99
-      WTHER = 'M'
-      
+      TYPE (ControlType) CONTROL
+
+!-----------------------------------------------------------------------
+!    Initialize
+!-----------------------------------------------------------------------      
+      SDATE  = -99
+      PDATE  = -99
+      MEWTH  = 'M'
+      RNMODE = CONTROL % RNMODE
 !-----------------------------------------------------------------------
 !    Open FILEX and read Weather station characters (WSTA)
 !-----------------------------------------------------------------------
@@ -88,9 +86,13 @@
               CALL IGNORE2(LUNEXP,LINEXP,ISECT,CHARTEST)
               
               IF(ISECT .EQ. 1) THEN
-                READ (CHARTEST,'(I3,9X,A8)', IOSTAT=ERRNUM) TLNFLD,WSTA
+                READ (CHARTEST,'(I3,9X,2A4)', IOSTAT=ERRNUM) TLNFLD,WSTA,WSTA1
                 IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY,4,FILEX,LINEXP)
                 IF (WSTA(1:1) .EQ. BLANK) CALL ERROR (ERRKEY,3,FILEX,LINEXP)
+                DO I = 1, 4
+                  WSTA(I:I)  = UPCASE(WSTA(I:I))
+                  WSTA1(I:I) = UPCASE(WSTA1(I:I))
+                END DO
               ELSE
                 CALL ERROR (ERRKEY,3,FILEX,LINEXP)
               ENDIF
@@ -150,16 +152,16 @@
               CALL ERROR (ERRKEY,6,FILEX,LNUM)  
             ENDIF
             
-            !FO - Find the specific header to search for WTHER
+            !FO - Find the specific header to search for MEWTH
             FINDH ='N MET'
             CALL HFIND(LUNEXP,FINDH,LINEXP,IFIND)
             IF(IFIND .EQ. 1) THEN
               
               CALL IGNORE2(LUNEXP,LINEXP,ISECT,CHARTEST)  
               IF(ISECT .EQ. 1) THEN
-                READ (CHARTEST,'(I2,17X,A1)', IOSTAT=ERRNUM) TLNSIM,WTHER
+                READ (CHARTEST,'(I2,17X,A1)', IOSTAT=ERRNUM) TLNSIM,MEWTH
                 IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY,11,FILEX,LNUM)
-                IF (WTHER .EQ. BLANK) CALL ERROR (ERRKEY,10,FILEX,LINEXP)            
+                IF (MEWTH .EQ. BLANK) CALL ERROR (ERRKEY,10,FILEX,LINEXP)            
               ELSE
                 CALL ERROR (ERRKEY,10,FILEX,LNUM)
               ENDIF
@@ -235,98 +237,29 @@
       
       !FO - Converts YRDOY to YR and DOY  
       YR  = INT(YRDOY / 1000)
-      DOY = YRDOY - YR * 1000   
-      
+      DOY = YRDOY - YR * 1000
+
 !-----------------------------------------------------------------------
-!    Create WTH file name and path
-!-----------------------------------------------------------------------
-!    Establish the weather file FILEW as WSTA + .WT?  where ? :
-!          M = observed data
-!          G = generated data
-!          S = interactively generated
-!-----------------------------------------------------------------------
-      IF(WTHER .EQ. 'M') THEN
-        IF (WSTA(5:5) .EQ. BLANK) THEN
-          WRITE (FILEW(1:12),'(A4,I2.2,A6)') WSTA,YR,'01.WTH'
-        ELSE
-          WRITE (FILEW(1:12),'(A8,A4)') WSTA,'.WTH'
-        ENDIF
-        PROCOD = 'WED'
-      ELSEIF(WTHER .EQ. 'G') THEN
-        
-        IF (WSTA(5:5) .EQ. BLANK) THEN
-          WRITE (FILEW(1:12),'(A4,I2.2,A6)') WSTA,YR,'01.WTG'
-        ELSE
-          WRITE (FILEW(1:12),'(A8,A4)') WSTA,'.WTG'
-        ENDIF
-        PROCOD = 'WGD'
-      ELSEIF (WTHER .EQ. 'S' .OR. WTHER .EQ. 'W') THEN
-         WRITE (FILEW(1:12),'(A4,A8)') WSTA,'.CLI    '
-         PROCOD = 'CLD'
-      ELSE
-         CALL ERROR (ERRKEY,16,FILEX,LINEXP)
-      ENDIF
-            
-!     Check weather filename in current directory
-      INQUIRE (FILE = FILEW,EXIST = FEXIST)
-      IF (FEXIST) THEN
-        PATHWT = BLANK
-!     Check weather filename in data directory
-      ELSE
-        FILETMP = TRIM(PATHEX)//FILEW
-        INQUIRE (FILE = FILETMP,EXIST = FEXIST)
-        IF (FEXIST) THEN
-          PATHWT = TRIM(PATHEX)
-!       Check weather filename in default DSSAT directory
-        ELSE
-          CALL PATH(PROCOD,DSSATP,PATHWT,1,NAMEF)
-          FILETMP = TRIM(PATHWT) // FILEW
-          INQUIRE (FILE=FILETMP, EXIST = FEXIST)
-          IF (FEXIST) THEN
-            PATHWT = PATHWT
-!         Check 4-character file name in data directory
-          ELSE
-            FILEW4 = FILEW(1:4) // ".WTH"
-            FILETMP = TRIM(PATHEX) // FILEW4
-            INQUIRE (FILE=FILETMP, EXIST = FEXIST)
-            IF (FEXIST) THEN
-              PATHWT = TRIM(PATHEX)
-              FILEW = FILEW4
-!           Check 4-character filename in default DSSAT directory
-            ELSE
-              FILEW4 = FILEW(1:4) // ".WTH"
-              FILETMP = TRIM(PATHWT) // FILEW4
-              INQUIRE (FILE=FILETMP, EXIST = FEXIST)
-              IF (FEXIST) THEN
-                PATHWT = TRIM(PATHWT)
-                FILEW = FILEW4
-              ELSE
-                MSG(1) = "Weather file not found."
-                MSG(2) = "  Neither " // FILEW // " nor " // FILEW4
-                MSG(3) = "  were found in weather or experiment directories."
-                MSG(4) = "Simulation will end."
-                CALL WARNING(4,ERRKEY,MSG)
-                CALL ERROR(ERRKEY,18,FILEW,0)
-              ENDIF
-            ENDIF
-          ENDIF
-        ENDIF
-      ENDIF
-      
+!     Check for Weather File in the directory
+!-----------------------------------------------------------------------    
+      CALL WTHFDIR(CONTROL,MEWTH,FILEX,WSTA,WSTA1,YRDOY,      & ! Input
+                   DSSATP,PATHEX,                             & ! Input
+                   FILEW, FILEWG, FILEWC,                     & ! Output
+                   FILEWTH, PATHWT, PATHWTC, PATHWTG, PATHWTW)  ! Output
 !-----------------------------------------------------------------------
 !    Open and Read Weather file data
 !-----------------------------------------------------------------------
       PATHL  = INDEX(PATHWT,BLANK)
       IF (PATHL .LE. 1) THEN
-         FILEWW = FILEW
+         FILEWW = FILEWTH
        ELSE
-         FILEWW = PATHWT(1:(PATHL-1)) // FILEW
+         FILEWW = PATHWT(1:(PATHL-1)) // FILEWTH
       ENDIF
       
       !FO - Process FILEW if last FILEW is different.
-      IF(LastFileW /= FILEW) THEN
+      IF(LastFileW /= FILEWTH) THEN
       
-        LastFileW = FILEW
+        LastFileW = FILEWTH
         
         ICASAF  = 0
         YEAR    = -99
@@ -338,9 +271,9 @@
         INQUIRE(FILE=FILEWW, EXIST = FEXIST)
         IF(FEXIST) THEN
           OPEN (UNIT, FILE = FILEWW,STATUS = 'OLD',IOSTAT=ERRNUM)
-          IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY,18,FILEW,0)
+          IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY,18,FILEWTH,0)
         ELSE
-          CALL ERROR (ERRKEY,18,FILEW,0)
+          CALL ERROR (ERRKEY,18,FILEWTH,0)
         ENDIF
               
         DO WHILE (ERRNUM .EQ. 0)
@@ -357,13 +290,13 @@
             IF(LINE(1:1) .EQ. '@' .AND. INDEX(LINE, 'DATE') .GT. 0) THEN
                 !FO - Read 7-digit First Weather YEAR
                 READ (UNIT,'(I7)', IOSTAT=ERRNUM) YEARDOY
-                IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY,17,FILEW,0)
+                IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY,17,FILEWTH,0)
                 
                 EXIT
             ELSE IF(LINE(1:1) .EQ. '@' .AND. INDEX(LINE, 'YEAR') .GT. 0) THEN
                 !FO - Read ICASA format
-                READ (UNIT,'(I4,1X,I3)', IOSTAT=ERRNUM) YEAR, DOY 
-                IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY,17,FILEW,0)
+                READ (UNIT,'(I5,1X,I3)', IOSTAT=ERRNUM) YEAR, DOY 
+                IF (ERRNUM .NE. 0) CALL ERROR (ERRKEY,17,FILEWTH,0)
                 
                 YEARDOY = YEAR * 1000 + DOY
                 
@@ -406,5 +339,5 @@
       
       ENDIF
       
-    END SUBROUTINE MAKEFILEW
+    END SUBROUTINE CHECK_Y4K_WTH
 
