fede451ca57f2f5872db874921df06e33af8ce91
Thiago Berton Ferreira <thiago.bertonf@gmail.com>
2026-04-17 16:19:15 -0400

    Fixed unit description on STEMP_EPIC

--- SPAM/STEMP_EPIC.for
+++ SPAM/STEMP_EPIC.for
@@ -432,20 +432,20 @@ C=======================================================================
 ! SOILPROP Composite variable containing soil properties including bulk
 !            density, drained upper limit, lower limit, pH, saturation
 !            water content.  Structure defined in ModuleDefs.
-! SRFTEMP  Temperature of soil surface litter (�C)
-! ST(L)    Soil temperature in soil layer L (�C)
+! SRFTEMP  Temperature of soil surface litter (°C)
+! ST(L)    Soil temperature in soil layer L (°C)
 ! SW(L)    Volumetric soil water content in layer L
 !           (cm3 [water] / cm3 [soil])
 ! SWI(L)   Initial soil water content (cm3[water]/cm3[soil])
 ! TAV      Average annual soil temperature, used with TAMP to calculate
-!            soil temperature. (�C)
-! TAVG     Average daily temperature (�C)
+!            soil temperature. (°C)
+! TAVG     Average daily temperature (°C)
 ! TBD      Sum of bulk density over soil profile
 ! TDL      Total water content of soil at drained upper limit (cm)
 ! TLL      Total soil water in the profile at the lower limit of
 !            plant-extractable water (cm)
-! TMA(I)   Array of previous 5 days of average soil temperatures. (�C)
-! TMAX     Maximum daily temperature (�C)
+! TMA(I)   Array of previous 5 days of average soil temperatures. (°C)
+! TMAX     Maximum daily temperature (°C)
 ! TSW      Total soil water in profile (cm)
 ! WC
 ! WW
